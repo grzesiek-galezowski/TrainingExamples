b5c9a469dba7b2bf63eee217fe324863c801a162
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-11-23 23:34:48 +0100

    .

---
+++ DealingWithNull/DealingWithNull/Maybe.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DealingWithNull
+{
+  public struct Maybe<T> : IEnumerable<T> where T : class
+  {
+    private readonly T _value;
+
+    public Maybe(T value)
+    {
+      _value = value;
+    }
+
+    public T Value
+    {
+      get
+      {
+        if (!HasValue)
+        {
+          throw new InvalidOperationException();
+        }
+        return _value;
+      }
+    }
+
+    public bool HasValue
+    {
+      get { return _value != null; }
+    }
+
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      if (HasValue)
+      {
+        yield return _value;
+      }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+
+    public static implicit operator Maybe<T>(T value)
+    {
+      return new Maybe<T>(value);
+    }
+  }
+}
---
+++ DealingWithNull/DealingWithNull/MaybeSpecification.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace DealingWithNull
+{
+  public class MaybeSpecification
+  {
+    [Test]
+    public void ShouldBEHAVIOR()
+    {
+      var maybeNothing = ReturnNull();
+      Assert.AreEqual(false, maybeNothing.HasValue);
+      Assert.AreEqual(default(Maybe<string>), maybeNothing);
+      foreach (var value in maybeNothing)
+      {
+        Assert.Fail("Should not have any value");
+      }
+
+      var maybeString = ReturnEmptyString();
+      Assert.AreEqual(true, maybeString.HasValue);
+      Assert.AreNotEqual(default(Maybe<string>), maybeString);
+    }
+
+    private static Maybe<string> ReturnNull()
+    {
+      return null;
+    }
+
+    private static Maybe<string> ReturnEmptyString()
+    {
+      return string.Empty;
+    }
+  }
+}
