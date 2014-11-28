531c263b34eab4c66e481d87068b5fcdb149c953
grzesiek <spectral@op.pl>
2014-11-28 10:25:19 +0100

    Added solution for dependency injection

---
+++ DependencyInjection/DependencyInjection/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    public class Class1
+    {
+    }
+
+  //TODO 3rd Party Connect
+  //TODO Composition Root
+  //TODO 
+  //TODO wrong use of DI container (
+  //container as singleton, 
+  //container used everywhere, 
+  //container used only in constructors, 
+  //container in composition root, 
+  //container as a factory
+  //TODO good use of DI container
+  //TODO DI container - good and bad
+  //TODO Composition Root DSL
+  //TODO managing disposables with DI
+  //TODO register-resolve-release
+  //TODO DI - cycles - 2 solutions (1. Passing to one side in method arguments instead of constructor, 2. Events/setters)
+}
