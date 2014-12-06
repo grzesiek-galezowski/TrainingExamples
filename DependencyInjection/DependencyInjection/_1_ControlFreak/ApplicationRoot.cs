a42870c053fc67f6f66eba5f3cdebb98452a46cd
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2014-12-06 22:02:57 +0100

    Added dependencies for training

--- DependencyInjection/DependencyInjection/_1_ControlFreak/ApplicationRoot.cs
+++ DependencyInjection/DependencyInjection/_1_ControlFreak/ApplicationRoot.cs
@@ -12,19 +12,4 @@ namespace DependencyInjection._1_ControlFreak
       }
     }
 
-  //TODO 3rd Party Connect
-  //TODO Composition Root
-  //TODO 
-  //TODO wrong use of DI container (
-  //container as singleton, 
-  //container used everywhere, 
-  //container used only in constructors, 
-  //container in composition root, 
-  //container as a factory
-  //TODO good use of DI container
-  //TODO DI container - good and bad
-  //TODO Composition Root DSL
-  //TODO managing disposables with DI
-  //TODO register-resolve-release
-  //TODO DI - cycles - 2 solutions (1. Passing to one side in method arguments instead of constructor, 2. Events/setters)
 }
