8ea21d7d0a837fc394e20e227f4e9541967a76d4
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2015-04-29 23:59:21 +0200

    Added temp solution - looking for new place

---
+++ TEMP/AtmaFileSystem/AtmaFileSystemSpecification/Class1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddEbook.TddToolkit;
+using Xunit;
+
+namespace AtmaFileSystemSpecification
+{
+  public class PathWithFileNameSpecification
+  {
+
+    [Fact]
+    public void ShouldNotAllowToBeCreatedWithNullValue()
+    {
+      Assert.Throws<ArgumentNullException>(() => PathWithFileName.To(null));
+    }
+
+    [Fact]
+    public void ShouldReturnNonNullPathToFileNameWhenCreatedWithWellFormedPathString()
+    {
+      Assert.NotNull(PathWithFileName.To(@"c:\\lolek\\lolki2.txt"));
+    }
+
+    [Fact]
+    public void ShouldThrowArgumentExceptionWhenTryingToCreateInstanceWithNotWellFormedUri()
+    {
+      Assert.Throws<ArgumentException>(() => PathWithFileName.To(@"C:\?||\|\\|\"));
+    }
+
+    [Fact]
+    public void ShouldBehaveLikeValueObject()
+    {
+      XAssert.IsValue<PathWithFileName>();
+    }
+
+
+
+  }
+
+  public class PathWithFileName : IEquatable<PathWithFileName>
+  {
+    public bool Equals(PathWithFileName other)
+    {
+      if (ReferenceEquals(null, other)) return false;
+      if (ReferenceEquals(this, other)) return true;
+      return string.Equals(_path, other._path);
+    }
+
+    public override bool Equals(object obj)
+    {
+      if (ReferenceEquals(null, obj)) return false;
+      if (ReferenceEquals(this, obj)) return true;
+      if (obj.GetType() != this.GetType()) return false;
+      return Equals((PathWithFileName) obj);
+    }
+
+    public override int GetHashCode()
+    {
+      return _path.GetHashCode();
+    }
+
+    public static bool operator ==(PathWithFileName left, PathWithFileName right)
+    {
+      return Equals(left, right);
+    }
+
+    public static bool operator !=(PathWithFileName left, PathWithFileName right)
+    {
+      return !Equals(left, right);
+    }
+
+    private readonly string _path;
+
+    public PathWithFileName(string path)
+    {
+      _path = path;
+    }
+
+    public static PathWithFileName To(string path)
+    {
+      if (null == path)
+      {
+        throw new ArgumentNullException(path);
+      }
+
+      if (!Path.IsPathRooted(path))
+      {
+        throw new ArgumentException(path);
+      }
+
+      else return new PathWithFileName(path);
+    }
+  }
+}
