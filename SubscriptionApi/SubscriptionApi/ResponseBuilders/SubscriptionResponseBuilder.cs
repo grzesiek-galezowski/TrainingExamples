6a81e41ebd1d287180c147f0c6c8690f2db6a9a1
Grzegorz Gałęzowski <robocz@wp.pl>
2016-03-31 18:30:24 +0200

    moved class to namespace

--- SubscriptionApi/SubscriptionApi/ResponseBuilders/SubscriptionResponseBuilder.cs
+++ SubscriptionApi/SubscriptionApi/ResponseBuilders/SubscriptionResponseBuilder.cs
@@ -36,6 +36,7 @@ namespace SubscriptionApi.ResponseBuilders
         Errors = _errors.ToArray()
       };
     }
+
     public StopSubscriptionResponseDto Build()
     {
       return new StopSubscriptionResponseDto()
