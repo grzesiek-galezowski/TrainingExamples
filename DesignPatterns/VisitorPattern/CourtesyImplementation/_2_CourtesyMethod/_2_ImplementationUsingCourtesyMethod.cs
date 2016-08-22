b4ef139d980763537383c227ee00084812775d76
grzesiek-galezowski <grzesiek.galezowski@gmail.com>
2016-08-22 21:02:09 +0200

    Added visitor example
    using Fowler's elephant example

---
+++ DesignPatterns/VisitorPattern/CourtesyImplementation/_2_CourtesyMethod/_2_ImplementationUsingCourtesyMethod.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace CourtesyImplementation2
+{
+  public interface Node
+  {
+    int GetElephantsCountInside();
+  }
+
+  public class Elephant : Node
+  {
+    public int GetElephantsCountInside()
+    {
+      return 1;
+    }
+  }
+
+  public class Box : Node
+  {
+    private readonly Node[] _content;
+
+    public Box(params Node[] content)
+    {
+      _content = content;
+    }
+
+    public int GetElephantsCountInside()
+    {
+      return _content.Sum(b => b.GetElephantsCountInside());
+    }
+  }
+
+
+  public class Lol
+  {
+    [Fact]
+    public void Whatever_Anything()
+    {
+      //GIVEN
+      var box = new Box(
+        new Box(
+          new Elephant()
+        ), 
+        new Box(
+          new Box(
+            new Elephant(),
+            new Elephant(),
+            new Elephant()
+          ), 
+          new Elephant()
+        )
+      );
+      //WHEN
+      var count = box.GetElephantsCountInside();
+
+      //THEN
+      count.Should().Be(5);
+    }
+  }
+
+}
---
+++ DesignPatterns/VisitorPattern/VisitorPattern/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+  public interface Node
+  {
+    int GetElephantsCountInside();
+  }
+
+  public class Elephant : Node
+  {
+    public int GetElephantsCountInside()
+    {
+      return 1;
+    }
+  }
+
+  public class Box : Node
+  {
+    private readonly Node[] _content;
+
+    public Box(Node[] content)
+    {
+      _content = content;
+    }
+
+    public int GetElephantsCountInside()
+    {
+      return _content.Sum(b => b.GetElephantsCountInside());
+    }
+  }
+
+}
