d2e997ec19fd4c2d612caa543433fbe3a598b37d
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2016-09-19 23:57:49 +0200

    Added initial state example

---
+++ DesignPatterns/State/State/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    public class Class1
+    {
+    //states are stateless - all fields read only
+    //state machine should be protected from threaded access
+    //if multithreaded, SM should be tell don't ask
+    //different ways of passing context
+    // - through setter
+    // - through getInitialState(this) and then each state passes context to another's factory method
+    // - through parameters
+    }
+}
