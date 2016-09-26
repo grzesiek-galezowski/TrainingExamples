57b4de7f14c88d1c79990535793733aaa9f6faa2
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2016-09-26 23:30:38 +0200

    Updated notes

--- DesignPatterns/State/State/Class1.cs
+++ DesignPatterns/State/State/Class1.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace State
+﻿namespace State
 {
     public class Class1
     {
-    //states are stateless - all fields read only
-    //state machine should be protected from threaded access
-    //if multithreaded, SM should be tell don't ask
-    //different ways of passing context
-    // - through setter
-    // - through getInitialState(this) and then each state passes context to another's factory method
-    // - through parameters
+      //states are stateless - all fields read only
+      //state machine should be protected from threaded access
+      //state machine should be thin - delegate all functionality
+      //if multithreaded, SM should be tell don't ask
+      //different ways of passing context
+      // - through setter
+      // - through getInitialState(this) and then each state passes context to another's factory method
+      // - through parameters
+      //two interfaces - signals and context
     }
 }
