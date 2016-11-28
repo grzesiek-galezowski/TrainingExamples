909dc375e36d12a63f167992416409ab6cf534df
Grzesiek Gałęzowski <grzesiek.galezowski@gmail.com>
2016-11-28 14:11:52 +0100

    Removed unnecessary files

--- Java/dependency-injection/bastard-injection/src/test/java/com/github/grzesiek_galezowski/test_environment/implementation_details/fixtures/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by astral on 28.03.2016.
- */
-
-@NonnullByDefault
-package com.github.grzesiek_galezowski.test_environment.implementation_details.fixtures;
-
-import com.sharneng.annotation.NonnullByDefault;
