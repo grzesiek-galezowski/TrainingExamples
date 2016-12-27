f08cadcbc33622b576904b0faafbd4ba297bb3e0
Grzesiek Gałęzowski <grzesiek.galezowski@gmail.com>
2016-12-27 23:49:36 +0100

    Adjusted sequence constructor example

---
+++ Cpp/Cpp11/InitializerLists/ConstructorNotation.cc
@@ -0,0 +1,35 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include<iostream>
+#include<initializer_list>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+class Object
+{
+public:
+  const int a;
+  const double b;
+  const char c;
+
+	explicit Object(int a, double b, char c) : a(a), b(b), c(c) {	}
+};
+
+TEST_CLASS(ConstructorNotation)
+{
+public:
+
+  TEST_METHOD(CustomConstructorInvocation)
+  {
+    Object o1{ 1, 2.5, 'c' };
+    //Object o2 = {1, 2.5, 'c'}; //comment out explicit for this to work
+
+    Assert().AreEqual(1, o1.a);
+    Assert().AreEqual(2.5, o1.b);
+    Assert().AreEqual('c', o1.c);
+  }
+
+};
+
+
---
+++ Cpp/Cpp11/InitializerLists/SequenceConstructor.cc
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include<iostream>
+#include<initializer_list>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+class Object
+{
+public:
+	Object(const std::initializer_list<int>& args)
+	{
+		for(std::initializer_list<int>::iterator it = args.begin(); it != args.end() ; ++it)
+		{
+			std::cout << *it;
+		}
+		std::cout << "\n";
+	}
+};
+
+TEST_CLASS(SequenceConstructorNotation)
+{
+public:
+
+  TEST_METHOD(SequenceConstructor)
+  {
+    Object o0({ 1, 2, 3, 4, 5, 7, 45, 3, 5 });
+    Object o1{ 1, 2, 3, 4, 5, 7, 45, 3, 5 };
+    Object o2 = { 1, 2, 3, 4, 5, 7, 45, 3, 5 };
+  }
+
+  TEST_METHOD(InitializerListType)
+  {
+    //It's just an object:
+    auto list2 = { 2.3, 4.5, 5.6, 6.7 };
+    Assert().AreEqual(
+      "class std::initializer_list<double>", typeid(list2).name());
+
+    std::initializer_list<double> list1{ 2.3, 4.5, 5.6, 6.7 };
+  }
+
+};
