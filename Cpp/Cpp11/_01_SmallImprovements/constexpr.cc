c06ecf7e5e54acf9bd2c928518c051aaa1a424d5
Grzesiek Gałęzowski <grzesiek.galezowski@gmail.com>
2016-12-27 22:58:54 +0100

    Improve first example

--- Cpp/Cpp11/_01_SmallImprovements/constexpr.cc
+++ Cpp/Cpp11/_01_SmallImprovements/constexpr.cc
@@ -10,73 +10,39 @@
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 
-namespace Microsoft
-{
-  namespace VisualStudio
-  {
-    namespace CppUnitTestFramework
-    {
-      template<> static std::wstring ToString<PowerOf2>(const PowerOf2& t) { 
-        RETURN_WIDE_STRING(static_cast<int>(t)); }
-    }
-  }
-}
-
-
 namespace _01_SmallImprovements
 {
-
-
-void print(vector<PowerOf2>& powers)
-{
-  for (PowerOf2& value : powers)
+  void print(vector<PowerOf2>& powers)
   {
-    cout << u8"Nastêpna potêga dwójki: " << endl;
+    for (PowerOf2& value : powers)
+    {
+      cout << u8"Nastêpna potêga dwójki: " << endl;
 
-    // std::cout << value << std::endl; //=> error!
+      // std::cout << value << std::endl; //=> error!
 
-    auto printablePower = static_cast<int>(value);
-    cout << printablePower << endl;
+      auto printablePower = static_cast<int>(value);
+      cout << printablePower << endl;
+    }
   }
 
-}
+  TEST_CLASS(UnitTest1)
+  {
+  public:
 
-int main()
-{
-  vector<PowerOf2> powers = {
-    PowerOf2::first
-    , PowerOf2::second
-    , PowerOf2::third
-    , PowerOf2::fourth
-    , PowerOf2::fifth
-  };
-
-  print(powers);
-
-  return 0;
-}
-
-
-	TEST_CLASS(UnitTest1)
-	{
-	public:
-		
     TEST_METHOD(CorrectValuesOfConstexprPow)
     {
       Assert().AreEqual<PowerOf2>(PowerOf2::second, PowerOf2::second);
-      Assert().AreNotEqual<PowerOf2>(PowerOf2::fifth, PowerOf2::first);
+      Assert().AreNotEqual<PowerOf2>(PowerOf2::third, PowerOf2::first);
 
       Assert().AreEqual(2, static_cast<int>(PowerOf2::first));
       Assert().AreEqual(4, static_cast<int>(PowerOf2::second));
       Assert().AreEqual(8, static_cast<int>(PowerOf2::third));
       Assert().AreEqual(16, static_cast<int>(PowerOf2::fourth));
       Assert().AreEqual(32, static_cast<int>(PowerOf2::fifth));
-
-
     }
 
-		TEST_METHOD(InitializerLists)
-		{
+    TEST_METHOD(InitializerLists)
+    {
       vector<PowerOf2> constexpr_powers = {
         PowerOf2::first
         , PowerOf2::second
@@ -85,8 +51,8 @@ int main()
         , PowerOf2::fifth
       };
 
+      print(constexpr_powers);
+    }
 
-		}
-
-	};
+  };
 }
--- Cpp/Cpp11/_01_SmallImprovements/constexpr.h
+++ Cpp/Cpp11/_01_SmallImprovements/constexpr.h
@@ -1,3 +1,23 @@
 #pragma once
 
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace Microsoft
+{
+  namespace VisualStudio
+  {
+    namespace CppUnitTestFramework
+    {
+      template<> static std::wstring ToString<PowerOf2>(const PowerOf2& t) {
+        RETURN_WIDE_STRING(static_cast<int>(t));
+      }
+    }
+  }
+}
+
+
 int main();
