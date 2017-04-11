a2a9142234635d2fba7cf69e20bfa0158cf4c7a9
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2017-04-11 08:29:51 +0200

    Added part of day randori code

---
+++ Java/day_night_randori/src/main/java/production/MutablePartOfDay.java
@@ -0,0 +1,21 @@
+package production;
+
+import java.time.LocalTime;
+
+// you can change constructors, add methods etc.
+// but you cannot remove or change the signatures of initialize and notifyOn
+public class MutablePartOfDay {
+    public void initialize(LocalTime time) {
+        //only called at beginning,
+        //no checks, sets the time passed
+        //does not notify anobody
+
+    }
+
+    public void notifyOn(LocalTime time) {
+        //changes the current part of day or stays at the same
+        //if change is made, notifies the weather server
+        //if a part of day is skipped, raises an error passing current part and received time
+    }
+}
+
---
+++ Java/day_night_randori/src/main/java/thirdparty/PartOfDayValue.java
@@ -0,0 +1,8 @@
+package thirdparty;
+
+public enum PartOfDayValue {
+    MORNING,   //0:00-12:00
+    AFTERNOON, //12:00-17:00
+    EVENING,   //17:00-21:00
+    NIGHT      //21:00-24:00
+}
