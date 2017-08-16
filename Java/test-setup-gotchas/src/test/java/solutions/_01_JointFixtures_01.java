5d6221e92abec08fb0cd009396eb1ab9d38abd77
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2017-08-16 12:24:58 +0200

    Added placeholder for solutions

---
+++ Java/test-setup-gotchas/src/test/java/solutions/_01_JointFixtures_01.java
@@ -0,0 +1,51 @@
+package solutions;
+
+import autofixture.publicinterface.Any;
+import lombok.val;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class _01_JointFixtures_01 {
+    private List<Integer> emptyList;
+    private ArrayList<Integer> listWithElement;
+    private Integer anyNumber;
+
+    @BeforeMethod
+    public void setUp() {
+        //A
+        emptyList = new ArrayList<>();
+
+        //B
+        anyNumber = Any.intValue();
+        listWithElement = new ArrayList<>();
+        listWithElement.add(anyNumber);
+    }
+
+    @Test //A
+    public void ShouldHaveElementCountOf0AfterBeingCreated() {
+        assertThat(emptyList.size()).isEqualTo(0);
+    }
+
+    @Test //A
+    public void ShouldBeEmptyAfterBeingCreated() {
+        assertThat(emptyList.isEmpty()).isTrue();
+    }
+
+    @Test //B
+    public void ShouldNotContainElementRemovedFromIt() {
+        listWithElement.remove(anyNumber);
+        assertThat(listWithElement.contains(anyNumber)).isFalse();
+    }
+
+    @Test //B
+    public void ShouldIncrementElementCountEachTimeTheSameElementIsAdded() {
+        val previousCount = listWithElement.size();
+        listWithElement.add(anyNumber);
+        assertThat(listWithElement.size()).isEqualTo(previousCount + 1);
+    }
+}
--- Java/test-setup-gotchas/src/test/java/solutions/_02_LocallyCorrectedFixture.java
+++ Java/test-setup-gotchas/src/test/java/solutions/_02_LocallyCorrectedFixture.java
@@ -1,4 +1,4 @@
-package problems;
+package solutions;
 
 import autofixture.publicinterface.Any;
 import lombok.val;
