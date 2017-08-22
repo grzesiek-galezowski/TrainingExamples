cddcb6c6bb59aca81838127aa4e5188ea9cb843c
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2017-08-22 20:00:49 +0200

    Added more comments

--- Java/automated-refactorings/src/main/java/_05_WrapReturn_MakeStatic_ConvertToInstance/EmployeeReportCommand.java
+++ Java/automated-refactorings/src/main/java/_05_WrapReturn_MakeStatic_ConvertToInstance/EmployeeReportCommand.java
@@ -13,6 +13,13 @@ public class EmployeeReportCommand {
   //todo apply to report
   //todo apply to employee dto
   //todo apply to employees
+  //1. extract method
+  //2. wrap return value
+  //3. extract variable
+  //4. inline
+  //5. Extract method
+  //6. Make static
+  //7. Convert to instance method
   public void execute() {
     final ArrayList<EmployeeDto> employeeDtos = Database.loadEmployees();
     String report = "";
