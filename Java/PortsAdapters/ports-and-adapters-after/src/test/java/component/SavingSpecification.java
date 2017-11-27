48e814eeca4175c0128110eefe70e0330bfd09f2
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2017-11-27 10:02:57 +0100

    Added IntelliJ default iml files

--- Java/PortsAdapters/ports-and-adapters-after/src/test/java/component/SavingSpecification.java
+++ Java/PortsAdapters/ports-and-adapters-after/src/test/java/component/SavingSpecification.java
@@ -3,27 +3,24 @@ package component;
 import com.example.application.ApplicationLogic;
 import com.example.ports.dataaccess.Repository;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 
 public class SavingSpecification {
+  //bug this is not showing how to write good component specs
+  //bug this is just an example showing that you can do this.
 
-  public class TheOnlyPathSpecification {
-    @Test
-    //bug this is not showing how to write good component specs
-    //bug this is just an example showing that you can do this.
-    public void shouldSaveEmployeeInDatabaseWhenTriggeredFromView() {
-      //GIVEN
-      Repository persistentStorage = Mockito.mock(Repository.class);
-      ApplicationLogic app = new ApplicationLogic(persistentStorage);
+  @Test
+  public void shouldSaveEmployeeInDatabaseWhenTriggeredFromView() {
+    //GIVEN
+    Repository persistentStorage = mock(Repository.class);
+    ApplicationLogic app = new ApplicationLogic(persistentStorage);
 
-      //THEN
-      app.handleAddEmployeeRequest();
+    //THEN
+    app.handleAddEmployeeRequest();
 
-      //THEN
-      then(persistentStorage).should().saveEmployee();
-    }
+    //THEN
+    then(persistentStorage).should().saveEmployee();
   }
-
 }
