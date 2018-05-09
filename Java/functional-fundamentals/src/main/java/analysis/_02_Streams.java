06c48dfaa278098f72f99682eacf234bafd41ccb
Galezowski Grzegorz <grzesiek.galezowski@gmail.com>
2018-05-09 12:49:26 +0200

    formatting

--- Java/functional-fundamentals/src/main/java/analysis/_02_Streams.java
+++ Java/functional-fundamentals/src/main/java/analysis/_02_Streams.java
@@ -15,8 +15,7 @@ public class _02_Streams {
                 .map(s -> s + ".")      // Arrays.asList("Ewa ma psa.", "Kot ma psa.", "Hubert ma psa.", "Ala ma psa.", "Julka ma psa.").stream()
                 .skip(1)                // Arrays.asList("Kot ma psa.", "Hubert ma psa.", "Ala ma psa.", "Julka ma psa.").stream()
                 .sorted()               // Arrays.asList("Ala ma psa.", "Hubert ma psa.", "Julka ma psa.", "Kot ma psa.").stream()
-                .map(s -> s.replace("psa", "kota"))
-                // Arrays.asList("Ala ma kota.", "Hubert ma kota.", "Julka ma kota.", "Kot ma kota.").stream()
+                .map(s -> s.replace("psa", "kota")) // Arrays.asList("Ala ma kota.", "Hubert ma kota.", "Julka ma kota.", "Kot ma kota.").stream()
                 .limit(3)               // Arrays.asList("Ala ma kota.", "Hubert ma kota.", "Julka ma kota.").stream()
                 .findFirst()            // "Ala ma kota."
                 .orElse("Ups! Zapomniałem elementarza.");
