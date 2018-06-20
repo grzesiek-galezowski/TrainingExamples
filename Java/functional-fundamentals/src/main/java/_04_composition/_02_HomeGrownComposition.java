8cd6d54cce3a4ececdbe1ea0e38ab2d0c027a30f
Galezowski Grzegorz <grzesiek.galezowski@gmail.com>
2018-06-20 14:20:07 +0200

    Added lazy evaluation step

--- Java/functional-fundamentals/src/main/java/_04_composition/_02_HomeGrownComposition.java
+++ Java/functional-fundamentals/src/main/java/_04_composition/_02_HomeGrownComposition.java
@@ -3,6 +3,7 @@ package _04_composition;
 import _04_composition.wrappers.Arithmetics;
 
 import static _04_composition.wrappers.Compose.compose;
+import static _04_composition.wrappers.Functions.f0;
 import static java.lang.System.out;
 
 public class _02_HomeGrownComposition {
@@ -10,9 +11,12 @@ public class _02_HomeGrownComposition {
 
         //DESCRIBE HOMEGROWN compose FUNCTION
 
-        //WITHOUT COMPOSITION
+        //WITHOUT COMPOSITION - EVALUATION
         out.println(0 + 1 + 1 + 1 + 1);
         out.println((((0 + 1) + 1) + 1) + 1);
+
+        //WITHOUT COMPOSITION - LAZY EVALUATION
+        out.println(f0(() -> (((0 + 1) + 1) + 1) + 1).apply());
 
         //SIMPLE FUNCTION COMPOSITION
         out.println(compose(Arithmetics::plus1, Arithmetics::plus1).apply(0));
