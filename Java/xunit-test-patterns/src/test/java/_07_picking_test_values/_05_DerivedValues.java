d1b203c33e3d1619f5644ecca54edf92b5aa27c9
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2018-06-26 00:01:10 +0200

    added example in wrong place

--- Java/xunit-test-patterns/src/test/java/_07_picking_test_values/_05_DerivedValues.java
+++ Java/xunit-test-patterns/src/test/java/_07_picking_test_values/_05_DerivedValues.java
@@ -1,4 +1,27 @@
 package _07_picking_test_values;
 
+import autofixture.publicinterface.Any;
+import autofixture.publicinterface.InstanceOf;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class _05_DerivedValues {
+
+    @Test
+    public void shouldCreateGenericInstances() {
+        //WHEN
+        MyClass<Integer> anonymous = Any.anonymous(new InstanceOf<MyClass<Integer>>() {});
+
+        //THEN
+        assertThat(anonymous.instance).isNotEqualTo(2); //TODO change to isEqualTo()
+    }
+
+    class MyClass<T> {
+        public T instance;
+
+        public MyClass(T instance) {
+            this.instance = instance;
+        }
+    }
 }
