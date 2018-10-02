27a23ada6479725d716cad17bf01a83db15bd5c2
Galezowski Grzegorz <grzesiek.galezowski@gmail.com>
2018-10-02 08:17:28 +0200

    more iditomatic c#

--- CSharp/AutomatedRefactorings/AutomatedRefactorings/5_MakeNotStaticFavouriteTrick/EmployeeReportCommand.cs
+++ CSharp/AutomatedRefactorings/AutomatedRefactorings/5_MakeNotStaticFavouriteTrick/EmployeeReportCommand.cs
@@ -24,11 +24,11 @@ namespace AutomatedRefactorings._5_MakeNotStaticFavouriteTrick
 
   public class EmployeeReportCommand
   {
-    private int averagePay;
+    private readonly int _averagePay;
 
     public EmployeeReportCommand(int averagePay)
     {
-      this.averagePay = averagePay;
+      this._averagePay = averagePay;
     }
 
     //todo apply to report
@@ -40,7 +40,7 @@ namespace AutomatedRefactorings._5_MakeNotStaticFavouriteTrick
     //4. inline
     //5. Extract method
     //6. Make static
-    //7. Convert to instance method
+    //7. Make Non Static
     public void Execute()
     {
       var employeeDtos = Database.LoadEmployees();
@@ -50,22 +50,22 @@ namespace AutomatedRefactorings._5_MakeNotStaticFavouriteTrick
       report.Append("UNDERPAID EMPLOYEES").Append(Environment.NewLine);
 
       //underpaid employees
-      foreach (EmployeeDto e in employeeDtos) 
+      foreach (var employee in employeeDtos) 
       {
-        if (e.Pay < averagePay * 0.8)
+        if (employee.Pay < _averagePay * 0.8)
         {
-          report.Append(e).Append(Environment.NewLine);
+          report.Append(employee).Append(Environment.NewLine);
         }
       }
 
       report.Append("OVERPAID EMPLOYEES").Append(Environment.NewLine);
 
       //overpaid employees
-      foreach (EmployeeDto e in employeeDtos) 
+      foreach (var employee in employeeDtos) 
       {
-        if (e.Pay > averagePay * 1.2)
+        if (employee.Pay > _averagePay * 1.2)
         {
-          report.Append(e).Append(Environment.NewLine);
+          report.Append(employee).Append(Environment.NewLine);
         }
       }
 
