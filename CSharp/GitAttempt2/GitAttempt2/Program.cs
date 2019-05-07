b233a00995be21b8328739a77b4edae9c4b45f6b
Galezowski Grzegorz <grzesiek.galezowski@gmail.com>
2019-05-07 16:32:54 +0200

    Update Program.cs

--- CSharp/GitAttempt2/GitAttempt2/Program.cs
+++ CSharp/GitAttempt2/GitAttempt2/Program.cs
@@ -6,67 +6,100 @@ using LibGit2Sharp;
 
 namespace GitAttempt2
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var commitsPerPath = new Dictionary<string, int>();
-      using (var repo = new Repository(@"C:\Users\grzes\Documents\GitHub\nscan"))
-      {
-        var commits = repo.Commits.ToArray();
-        for (int i = 1; i < commits.Count(); ++i)
+        static void Main(string[] args)
         {
-          AddToChanges(repo, commitsPerPath, commits[i], commits[i - 1]);
-          PrintTreeOf(commits[i].Tree);
+            using (var repo = new Repository(@"c:\Users\ftw637\Documents\GitHub\TrainingExamples\"))
+            {
+                var analysisMetadata = ScanAllCommits(repo);
+
+
+
+                /*
+                foreach (var analysisMetadata in commitsPerPath)
+                {
+                    if (analysisMetadata.Value._aliases.Count > 1)
+                    {
+                        Console.WriteLine(analysisMetadata.Key.Sha);
+                        foreach (var valueAliase in analysisMetadata.Value._aliases)
+                        {
+                            Console.WriteLine("==>" + valueAliase);
+                        }
+                        Console.WriteLine(analysisMetadata.Value.ModifiedCommitsCount);
+                    }
+                }*/
+                
+                foreach (var keyValuePair in analysisMetadata.OrderBy(kvp => kvp.Value.ModifiedCommitsCount))
+                {
+                    Console.WriteLine(keyValuePair.Value.ModifiedCommitsCount);
+                }
+
+                PrintTreeOf(repo.Commits.Last().Tree);
+            }
 
         }
-      }
-      /*
-      foreach (var keyValuePair in commitsPerPath.OrderBy(kvp => kvp.Value))
-      {
-        Console.WriteLine($"{keyValuePair.Key} => {keyValuePair.Value}");
-      }*/
+
+        private static Dictionary<ObjectId, AnalysisMetadata> ScanAllCommits(Repository repo)
+        {
+            var commitsPerPath = new Dictionary<ObjectId, AnalysisMetadata>();
+            var commits = repo.Commits.ToArray();
+            for (var i = 1; i < commits.Length; ++i)
+            {
+                var previousCommit = commits[i - 1];
+                var currentCommit = commits[i];
+                var treeChanges = repo.Diff.Compare<TreeChanges>(currentCommit.Tree, previousCommit.Tree);
+
+                foreach (var treeEntry in treeChanges)
+                {
+                    if (!commitsPerPath.ContainsKey(treeEntry.Oid))
+                    {
+                        commitsPerPath[treeEntry.Oid] = new AnalysisMetadata();
+                    }
+
+                    commitsPerPath[treeEntry.Oid].AddAlias(treeEntry.Path);
+                    commitsPerPath[treeEntry.Oid].IncreaseModifiedCommittsCount();
+                }
+            }
+
+            return commitsPerPath;
+        }
+
+        private static void PrintTreeOf(Tree tree)
+        {
+            foreach (var treeEntry in tree)
+            {
+                switch (treeEntry.TargetType)
+                {
+                    case TreeEntryTargetType.Blob:
+                        Console.WriteLine(treeEntry.Path);
+                        break;
+                    case TreeEntryTargetType.Tree:
+                        Console.WriteLine(treeEntry.Path);
+                        PrintTreeOf((Tree) treeEntry.Target);
+                        break;
+                    case TreeEntryTargetType.GitLink:
+                        throw new ArgumentException(treeEntry.Path);
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
     }
 
-    private static void PrintTreeOf(Tree tree)
+    public class AnalysisMetadata
     {
-      foreach (var treeEntry in tree)
-      {
-        switch (treeEntry.TargetType)
-        {
-          case TreeEntryTargetType.Blob:
-            Console.WriteLine(treeEntry.Path);
-            break;
-          case TreeEntryTargetType.Tree:
-            Console.WriteLine(treeEntry.Path);
-            PrintTreeOf((Tree)treeEntry.Target);
-            break;
-          case TreeEntryTargetType.GitLink:
-            throw new ArgumentException(treeEntry.Path);
-          default:
-            throw new ArgumentOutOfRangeException();
-        }
-      }
-    }
+        public List<string> _aliases = new List<string>();
+        public int ModifiedCommitsCount { get; set; } = 0;
 
-    private static void AddToChanges(
-      IRepository repo, 
-      IDictionary<string, int> commitsPerPath, 
-      Commit currentCommit, 
-      Commit previousCommit)
-    {
-      var treeChanges = repo.Diff.Compare<TreeChanges>(currentCommit.Tree, previousCommit.Tree);
-      foreach (var treeEntry in treeChanges)
-      {
-        if (commitsPerPath.ContainsKey(treeEntry.Path))
+        public void AddAlias(string path)
         {
-          commitsPerPath[treeEntry.Path]++;
+            _aliases.Add(path);
         }
-        else
+
+        public void IncreaseModifiedCommittsCount()
         {
-          commitsPerPath[treeEntry.Path] = 1;
+            ModifiedCommitsCount++;
         }
-      }
     }
-  }
 }
