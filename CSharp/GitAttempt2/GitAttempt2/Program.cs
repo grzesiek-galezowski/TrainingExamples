c6ee7dbb9bafbffa2a7f6f7258c1149b987eaedd
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-05-09 00:09:11 +0200

    Update Program.cs

--- CSharp/GitAttempt2/GitAttempt2/Program.cs
+++ CSharp/GitAttempt2/GitAttempt2/Program.cs
@@ -7,179 +7,144 @@ using LibGit2Sharp;
 
 namespace GitAttempt2
 {
-    public class Map<T1, T2>
+
+  //bug use own ids instead of oids or paths. Both are not reliable
+  class Program
+  {
+    static void Main(string[] args)
     {
-        private Dictionary<T1, T2> _forward = new Dictionary<T1, T2>();
-        private Dictionary<T2, T1> _reverse = new Dictionary<T2, T1>();
+      //using var repo = new Repository(@"c:\Users\ftw637\Documents\GitHub\TrainingExamples\");
+      using var repo = new Repository(@"C:\Users\grzes\Documents\GitHub\nscan\");
+      var analysisMetadata = ScanAllCommits(repo);
 
-        public Map()
-        {
-            this.Forward = new Indexer<T1, T2>(_forward);
-            this.Reverse = new Indexer<T2, T1>(_reverse);
-        }
+      var pathsInTrunk = new List<string>();
+      CollectPathsFrom(repo.Branches["master"].Commits.Reverse().Last().Tree, pathsInTrunk);
 
-        public class Indexer<T3, T4>
-        {
-            private Dictionary<T3, T4> _dictionary;
-            public Indexer(Dictionary<T3, T4> dictionary)
-            {
-                _dictionary = dictionary;
-            }
-            public T4 this[T3 index]
-            {
-                get { return _dictionary[index]; }
-                set { _dictionary[index] = value; }
-            }
-        }
+      var trunkFiles = analysisMetadata.Where(am => pathsInTrunk.Contains(am.Key))
+        .Select(x => new TrunkFile(x.Value, x.Key));
 
-        public void Add(T1 t1, T2 t2)
-        {
-            _forward.Add(t1, t2);
-            _reverse.Add(t2, t1);
-        }
-
-        public Indexer<T1, T2> Forward { get; private set; }
-        public Indexer<T2, T1> Reverse { get; private set; }
+      foreach (var trunkFile in trunkFiles.OrderBy(f => f.Metadata.ModifiedCommitsCount))
+      {
+        Console.WriteLine(trunkFile.Path + " => " + trunkFile.Metadata.ModifiedCommitsCount);
+      }
     }
 
-    //bug use own ids instead of oids or paths. Both are not reliable
-    class Program
+    private static Dictionary<string, AnalysisMetadata> ScanAllCommits(Repository repo)
     {
-        private Map<string, ObjectId> oidToPath
+      var commitsPerPath = new Dictionary<string, AnalysisMetadata>();
+      var commits = repo.Branches["master"].Commits.Reverse().ToArray();
 
-        static void Main(string[] args)
-        {
-            //using var repo = new Repository(@"c:\Users\ftw637\Documents\GitHub\TrainingExamples\");
-            using var repo = new Repository(@"C:\Users\grzes\Documents\GitHub\TrainingExamples\");
-            var analysisMetadata = ScanAllCommits(repo);
+      Commit first = commits.First();
+      AddCommit(first.Tree, commitsPerPath);
+      for (var i = 1; i < commits.Length; ++i)
+      {
+        var previousCommit = commits[i - 1];
+        var currentCommit = commits[i];
 
-            var pathsByOid = new Dictionary<ObjectId, string>();
-            PrintTreeOf(repo.Branches["master"].Commits.Reverse().Last().Tree, pathsByOid);
+        AddFiles(repo.Diff.Compare<TreeChanges>(previousCommit.Tree, currentCommit.Tree), commitsPerPath);
+      }
 
-            var trunkFiles = analysisMetadata.Where(am => pathsByOid.ContainsKey(am.Key)).Select(x => new TrunkFile(x.Key, x.Value, pathsByOid[x.Key]));
-
-            foreach (var trunkFile in trunkFiles.OrderBy(f => f._metadata.ModifiedCommitsCount))
-            {
-                if (trunkFile._path.Contains("Controller"))
-                {
-                    Console.WriteLine(trunkFile._path + " => " + trunkFile._metadata.ModifiedCommitsCount);
-                }
-            }
-        }
-
-        private static Dictionary<ObjectId, AnalysisMetadata> ScanAllCommits(Repository repo)
-        {
-            var commitsPerPath = new Dictionary<ObjectId, AnalysisMetadata>();
-            var commits = repo.Branches["master"].Commits.Reverse().ToArray();
-
-            Commit first = commits.First();
-            AddCommit(first.Tree, commitsPerPath);
-            for (var i = 1; i < commits.Length; ++i)
-            {
-                var previousCommit = commits[i - 1];
-                var currentCommit = commits[i];
-
-                AddFiles(repo.Diff.Compare<TreeChanges>(previousCommit.Tree, currentCommit.Tree), commitsPerPath);
-            }
-
-            return commitsPerPath;
-        }
-
-        private static void AddCommit(Tree treeEntries, IDictionary<ObjectId, AnalysisMetadata> commitsPerPath)
-        {
-            foreach (var treeEntry in treeEntries)
-            {
-                switch (treeEntry.TargetType)
-                {
-                    case TreeEntryTargetType.Blob:
-                        if (!commitsPerPath.ContainsKey(treeEntry.Target.Id))
-                        {
-                            commitsPerPath[treeEntry.Target.Id] = new AnalysisMetadata();
-                        }
-
-                        commitsPerPath[treeEntry.Target.Id].AddAlias(treeEntry.Path);
-                        commitsPerPath[treeEntry.Target.Id].IncreaseModifiedCommittsCount();
-
-                        break;
-                    case TreeEntryTargetType.Tree:
-                        AddCommit((Tree) treeEntry.Target, commitsPerPath);
-                        break;
-                    case TreeEntryTargetType.GitLink:
-                        throw new ArgumentException(treeEntry.Path);
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-
-        }
-
-        private static void AddFiles(TreeChanges treeChanges, Dictionary<ObjectId, AnalysisMetadata> commitsPerPath)
-        {
-            foreach (var treeEntry in treeChanges)
-            {
-                if (!commitsPerPath.ContainsKey(treeEntry.Oid))
-                {
-                    if (treeEntry.Path.Contains("UsersController.cs"))
-                    {
-                        Console.WriteLine("YESSS " + treeEntry.Path + " " + treeEntry.Oid);
-                    }
-
-                    commitsPerPath[treeEntry.Oid] = new AnalysisMetadata();
-                }
-
-                commitsPerPath[treeEntry.Oid].AddAlias(treeEntry.Path);
-                commitsPerPath[treeEntry.Oid].IncreaseModifiedCommittsCount();
-            }
-        }
-
-        private static void PrintTreeOf(Tree tree, Dictionary<ObjectId, string> pathsByOid)
-        {
-            foreach (var treeEntry in tree)
-            {
-                switch (treeEntry.TargetType)
-                {
-                    case TreeEntryTargetType.Blob:
-                        pathsByOid[treeEntry.Target.Id] = treeEntry.Path;
-                        break;
-                    case TreeEntryTargetType.Tree:
-                        PrintTreeOf((Tree) treeEntry.Target, pathsByOid);
-                        break;
-                    case TreeEntryTargetType.GitLink:
-                        throw new ArgumentException(treeEntry.Path);
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-        }
+      return commitsPerPath;
     }
 
-    public class TrunkFile
+    private static void AddCommit(Tree treeEntries, Dictionary<string, AnalysisMetadata> commitsPerPath)
     {
-        public readonly ObjectId _id;
-        public readonly AnalysisMetadata _metadata;
-        public readonly string _path;
-
-        public TrunkFile(ObjectId id, AnalysisMetadata metadata, string path)
+      foreach (var treeEntry in treeEntries)
+      {
+        switch (treeEntry.TargetType)
         {
-            _id = id;
-            _metadata = metadata;
-            _path = path;
+          case TreeEntryTargetType.Blob:
+            if (!commitsPerPath.ContainsKey(treeEntry.Path))
+            {
+              commitsPerPath[treeEntry.Path] = new AnalysisMetadata();
+            }
+
+            commitsPerPath[treeEntry.Path].IncreaseModifiedCommitsCount();
+
+            break;
+          case TreeEntryTargetType.Tree:
+            AddCommit((Tree) treeEntry.Target, commitsPerPath);
+            break;
+          case TreeEntryTargetType.GitLink:
+            throw new ArgumentException(treeEntry.Path);
+          default:
+            throw new ArgumentOutOfRangeException();
         }
+      }
+
     }
 
-    public class AnalysisMetadata
+    private static void AddFiles(TreeChanges treeChanges, Dictionary<string, AnalysisMetadata> commitsPerPath)
     {
-        public List<string> _aliases = new List<string>();
-        public int ModifiedCommitsCount { get; set; } = 0;
-
-        public void AddAlias(string path)
+      foreach (var treeEntry in treeChanges)
+      {
+        switch (treeEntry.Status)
         {
-            _aliases.Add(path);
+          case ChangeKind.Unmodified:
+            break;
+          case ChangeKind.Added:
+            commitsPerPath[treeEntry.Path] = new AnalysisMetadata();
+            commitsPerPath[treeEntry.Path].IncreaseModifiedCommitsCount();
+            break;
+          case ChangeKind.Deleted:
+            break;
+          case ChangeKind.Modified:
+            commitsPerPath[treeEntry.Path].IncreaseModifiedCommitsCount();
+            break;
+          case ChangeKind.Renamed:
+            commitsPerPath[treeEntry.Path] = commitsPerPath[treeEntry.OldPath];
+            commitsPerPath[treeEntry.Path].IncreaseModifiedCommitsCount();
+            break;
+          case ChangeKind.Copied:
+            commitsPerPath[treeEntry.Path] = new AnalysisMetadata();
+            commitsPerPath[treeEntry.Path].IncreaseModifiedCommitsCount();
+            break;
+          default:
+            throw new ArgumentOutOfRangeException();
         }
-
-        public void IncreaseModifiedCommittsCount()
-        {
-            ModifiedCommitsCount++;
-        }
+      }
     }
+
+    private static void CollectPathsFrom(Tree tree, List<string> pathsByOid)
+    {
+      foreach (var treeEntry in tree)
+      {
+        switch (treeEntry.TargetType)
+        {
+          case TreeEntryTargetType.Blob:
+            pathsByOid.Add(treeEntry.Path);
+            break;
+          case TreeEntryTargetType.Tree:
+            CollectPathsFrom((Tree) treeEntry.Target, pathsByOid);
+            break;
+          case TreeEntryTargetType.GitLink:
+            throw new ArgumentException(treeEntry.Path);
+          default:
+            throw new ArgumentOutOfRangeException();
+        }
+      }
+    }
+  }
+
+  public class TrunkFile
+  {
+    public AnalysisMetadata Metadata { get; }
+    public string Path { get; }
+
+    public TrunkFile(AnalysisMetadata metadata, string path)
+    {
+      Metadata = metadata;
+      Path = path;
+    }
+  }
+
+  public class AnalysisMetadata
+  {
+    public int ModifiedCommitsCount { get; private set; } = 0;
+
+    public void IncreaseModifiedCommitsCount()
+    {
+      ModifiedCommitsCount++;
+    }
+  }
 }
