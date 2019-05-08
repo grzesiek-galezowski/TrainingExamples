af920333f61cee0f80b45482cc337e3d01b9d8c4
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-05-08 07:41:17 +0200

    Something works

--- CSharp/GitAttempt2/GitAttempt2/Program.cs
+++ CSharp/GitAttempt2/GitAttempt2/Program.cs
@@ -6,100 +6,122 @@ using LibGit2Sharp;
 
 namespace GitAttempt2
 {
-    class Program
+  //bug use own ids instead of oids or paths. Both are not reliable
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            using (var repo = new Repository(@"c:\Users\ftw637\Documents\GitHub\TrainingExamples\"))
-            {
-                var analysisMetadata = ScanAllCommits(repo);
+      //using var repo = new Repository(@"c:\Users\ftw637\Documents\GitHub\TrainingExamples\")
+      using var repo = new Repository(@"C:\Users\grzes\Documents\GitHub\TrainingExamples\");
+      var analysisMetadata = ScanAllCommits(repo);
 
-
-
-                /*
-                foreach (var analysisMetadata in commitsPerPath)
-                {
-                    if (analysisMetadata.Value._aliases.Count > 1)
-                    {
-                        Console.WriteLine(analysisMetadata.Key.Sha);
-                        foreach (var valueAliase in analysisMetadata.Value._aliases)
-                        {
-                            Console.WriteLine("==>" + valueAliase);
-                        }
-                        Console.WriteLine(analysisMetadata.Value.ModifiedCommitsCount);
-                    }
-                }*/
-                
-                foreach (var keyValuePair in analysisMetadata.OrderBy(kvp => kvp.Value.ModifiedCommitsCount))
-                {
-                    Console.WriteLine(keyValuePair.Value.ModifiedCommitsCount);
-                }
-
-                PrintTreeOf(repo.Commits.Last().Tree);
-            }
-
-        }
-
-        private static Dictionary<ObjectId, AnalysisMetadata> ScanAllCommits(Repository repo)
-        {
-            var commitsPerPath = new Dictionary<ObjectId, AnalysisMetadata>();
-            var commits = repo.Commits.ToArray();
-            for (var i = 1; i < commits.Length; ++i)
-            {
-                var previousCommit = commits[i - 1];
-                var currentCommit = commits[i];
-                var treeChanges = repo.Diff.Compare<TreeChanges>(currentCommit.Tree, previousCommit.Tree);
-
-                foreach (var treeEntry in treeChanges)
-                {
-                    if (!commitsPerPath.ContainsKey(treeEntry.Oid))
-                    {
-                        commitsPerPath[treeEntry.Oid] = new AnalysisMetadata();
-                    }
-
-                    commitsPerPath[treeEntry.Oid].AddAlias(treeEntry.Path);
-                    commitsPerPath[treeEntry.Oid].IncreaseModifiedCommittsCount();
-                }
-            }
-
-            return commitsPerPath;
-        }
-
-        private static void PrintTreeOf(Tree tree)
-        {
-            foreach (var treeEntry in tree)
-            {
-                switch (treeEntry.TargetType)
-                {
-                    case TreeEntryTargetType.Blob:
-                        Console.WriteLine(treeEntry.Path);
-                        break;
-                    case TreeEntryTargetType.Tree:
-                        Console.WriteLine(treeEntry.Path);
-                        PrintTreeOf((Tree) treeEntry.Target);
-                        break;
-                    case TreeEntryTargetType.GitLink:
-                        throw new ArgumentException(treeEntry.Path);
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-        }
+      var pathsByOid = new Dictionary<ObjectId, string>();
+      PrintTreeOf(repo.Branches["master"].Commits.Reverse().Last().Tree, pathsByOid);
+      
+      foreach (var entry in pathsByOid)
+      {
+        Console.WriteLine(entry.Value + " => " + analysisMetadata[entry.Key].ModifiedCommitsCount);
+      }
     }
 
-    public class AnalysisMetadata
+    private static Dictionary<ObjectId, AnalysisMetadata> ScanAllCommits(Repository repo)
     {
-        public List<string> _aliases = new List<string>();
-        public int ModifiedCommitsCount { get; set; } = 0;
+      var commitsPerPath = new Dictionary<ObjectId, AnalysisMetadata>();
+      var commits = repo.Branches["master"].Commits.Reverse().ToArray();
 
-        public void AddAlias(string path)
-        {
-            _aliases.Add(path);
-        }
+      Commit first = commits.First();
+      AddCommit(first.Tree, commitsPerPath);
+      for (var i = 1; i < commits.Length; ++i)
+      {
+        var previousCommit = commits[i - 1];
+        var currentCommit = commits[i];
 
-        public void IncreaseModifiedCommittsCount()
-        {
-            ModifiedCommitsCount++;
-        }
+        AddFiles(repo.Diff.Compare<TreeChanges>(previousCommit.Tree, currentCommit.Tree), commitsPerPath);
+      }
+
+      return commitsPerPath;
     }
+
+    private static void AddCommit(Tree treeEntries, Dictionary<ObjectId, AnalysisMetadata> commitsPerPath)
+    {
+      foreach (var treeEntry in treeEntries)
+      {
+        switch (treeEntry.TargetType)
+        {
+          case TreeEntryTargetType.Blob:
+            if (!commitsPerPath.ContainsKey(treeEntry.Target.Id))
+            {
+              commitsPerPath[treeEntry.Target.Id] = new AnalysisMetadata();
+            }
+
+            commitsPerPath[treeEntry.Target.Id].AddAlias(treeEntry.Path);
+            commitsPerPath[treeEntry.Target.Id].IncreaseModifiedCommittsCount();
+
+            break;
+          case TreeEntryTargetType.Tree:
+            AddCommit((Tree)treeEntry.Target, commitsPerPath);
+            break;
+          case TreeEntryTargetType.GitLink:
+            throw new ArgumentException(treeEntry.Path);
+          default:
+            throw new ArgumentOutOfRangeException();
+        }
+      }
+
+    }
+
+    private static void AddFiles(TreeChanges treeChanges, Dictionary<ObjectId, AnalysisMetadata> commitsPerPath)
+    {
+      foreach (var treeEntry in treeChanges)
+      {
+        if (!commitsPerPath.ContainsKey(treeEntry.Oid))
+        {
+          if (treeEntry.Path.Contains(".gitattributes"))
+          {
+            Console.WriteLine("YESSS");
+          }
+          commitsPerPath[treeEntry.Oid] = new AnalysisMetadata();
+        }
+
+        commitsPerPath[treeEntry.Oid].AddAlias(treeEntry.Path);
+        commitsPerPath[treeEntry.Oid].IncreaseModifiedCommittsCount();
+      }
+    }
+
+    private static void PrintTreeOf(Tree tree, Dictionary<ObjectId, string> pathsByOid)
+    {
+      foreach (var treeEntry in tree)
+      {
+        switch (treeEntry.TargetType)
+        {
+          case TreeEntryTargetType.Blob:
+            pathsByOid[treeEntry.Target.Id] = treeEntry.Path;
+            break;
+          case TreeEntryTargetType.Tree:
+            PrintTreeOf((Tree) treeEntry.Target, pathsByOid);
+            break;
+          case TreeEntryTargetType.GitLink:
+            throw new ArgumentException(treeEntry.Path);
+          default:
+            throw new ArgumentOutOfRangeException();
+        }
+      }
+    }
+  }
+
+  public class AnalysisMetadata
+  {
+    public List<string> _aliases = new List<string>();
+    public int ModifiedCommitsCount { get; set; } = 0;
+
+    public void AddAlias(string path)
+    {
+      _aliases.Add(path);
+    }
+
+    public void IncreaseModifiedCommittsCount()
+    {
+      ModifiedCommitsCount++;
+    }
+  }
 }
