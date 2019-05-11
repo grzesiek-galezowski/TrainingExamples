300f1a62f13e45d44de50d59b22b09a4784f4a17
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-05-11 16:57:52 +0200

    added prototype charts

--- CSharp/GitAttempt2/GitAttempt2/Program.cs
+++ CSharp/GitAttempt2/GitAttempt2/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using Chart.Mvc.ComplexChart;
+using Chart.Mvc.Extensions;
 
 namespace GitAttempt2
 {
@@ -15,6 +21,46 @@ namespace GitAttempt2
       foreach (var trunkFile in trunkFiles.OrderBy(f => f.ChangeRate * f.Complexity))
       {
         Console.WriteLine(trunkFile.Path + " => " + trunkFile.ChangeRate + ":" + trunkFile.Complexity);
+      }
+
+      RenderChart(trunkFiles);
+
+      //trunkFiles.First()
+    }
+
+    private static void RenderChart(IEnumerable<TrunkFile> trunkFiles)
+    {
+      var template = File.ReadAllText("template.html");
+      File.WriteAllText("output.html", template.Replace("___LABELS___", "'lol1', 'lol2'").Replace("___DATA___", "'2', '3'"));
+      OpenBrowser("output.html");
+    }
+
+    public static void OpenBrowser(string url)
+    {
+      try
+      {
+        Process.Start(url);
+      }
+      catch
+      {
+        // hack because of this: https://github.com/dotnet/corefx/issues/10361
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+          url = url.Replace("&", "^&");
+          Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+          Process.Start("xdg-open", url);
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+          Process.Start("open", url);
+        }
+        else
+        {
+          throw;
+        }
       }
     }
   }
