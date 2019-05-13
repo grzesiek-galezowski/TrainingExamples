538905e70a56b570cd736b6e520624a3ebff8fb9
Galezowski Grzegorz <grzesiek.galezowski@gmail.com>
2019-05-13 16:26:07 +0200

    Comparing file content without whitespaces

--- CSharp/GitAttempt2/ApplicationLogic/Change.cs
+++ CSharp/GitAttempt2/ApplicationLogic/Change.cs
@@ -1,50 +1,25 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace ApplicationLogic
 {
-    public sealed class Change : IEquatable<Change>
+    public sealed class Change
     {
-      public string Path { get; }
-      public DateTimeOffset ChangeDate { get; }
-      public string Text { get; }
-      public double Complexity { get; }
+        public string Path { get; }
+        public DateTimeOffset ChangeDate { get; }
+        public string Text { get; }
+        public double Complexity { get; }
 
-      public Change(string path, string text, double complexity, DateTimeOffset changeDate)
-      {
-        Path = path;
-        ChangeDate = changeDate;
-        Text = text;
-        Complexity = complexity;
-      }
+        public Change(string path, string text, double complexity, DateTimeOffset changeDate)
+        {
+            Path = path;
+            ChangeDate = changeDate;
+            Text = text;
+            Complexity = complexity;
+            TextWithoutWhitespaces = Regex.Replace(Text, @"\s+", "");
+        }
 
-      public bool Equals(Change other) //bug change these equals to something else
-      {
-        if (ReferenceEquals(null, other)) return false;
-        if (ReferenceEquals(this, other)) return true;
-        return string.Equals(Text, other.Text);
-      }
-
-      public override bool Equals(object obj)
-      {
-        if (ReferenceEquals(null, obj)) return false;
-        if (ReferenceEquals(this, obj)) return true;
-        if (obj.GetType() != this.GetType()) return false;
-        return Equals((Change) obj);
-      }
-
-      public override int GetHashCode()
-      {
-        return (Text != null ? Text.GetHashCode() : 0);
-      }
-
-      public static bool operator ==(Change left, Change right)
-      {
-        return Equals(left, right);
-      }
-
-      public static bool operator !=(Change left, Change right)
-      {
-        return !Equals(left, right);
-      }
+        public string TextWithoutWhitespaces { get; }
     }
 }
+    
--- CSharp/GitAttempt2/ApplicationLogic/ChangeLog.cs
+++ CSharp/GitAttempt2/ApplicationLogic/ChangeLog.cs
@@ -1,44 +1,46 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ApplicationLogic
 {
-  public class ChangeLog
-  {
-    private readonly List<Change> _entries = new List<Change>();
-
-    public IReadOnlyList<Change> Entries => _entries;
-
-    public void AddDataFrom(Change change)
+    public class ChangeLog
     {
-      if (!_entries.Any())
-      {
-        _entries.Add(change);
-      }
-      else if (!IsDuplicatedForSomeReason(change))
-      {
-        _entries.Add(change);
-      }
-    }
+        private readonly List<Change> _entries = new List<Change>();
 
-    private bool IsDuplicatedForSomeReason(Change change)
-    {
-      return _entries.Last().Text.Equals(change.Text);
-    }
+        public IReadOnlyList<Change> Entries => _entries;
 
-    public string PathOfLastVersion()
-    {
-      return Entries.Last().Path;
-    }
+        public void AddDataFrom(Change change)
+        {
+            if (!_entries.Any())
+            {
+                _entries.Add(change);
+            }
+            else if (!HasTheSameContent(change))
+            {
+                _entries.Add(change);
+            }
+        }
 
-    public int ChangesCount()
-    {
-      return Entries.Count;
-    }
+        private bool HasTheSameContent(Change change)
+        {
+            return _entries.Last().TextWithoutWhitespaces.Equals(change.TextWithoutWhitespaces);
+        }
 
-    public double ComplexityOfLastVersion()
-    {
-      return Entries.Last().Complexity;
+        public string PathOfLastVersion()
+        {
+            return Entries.Last().Path;
+        }
+
+        public int ChangesCount()
+        {
+            return Entries.Count;
+        }
+
+        public double ComplexityOfLastVersion()
+        {
+            return Entries.Last().Complexity;
+        }
     }
-  }
 }
