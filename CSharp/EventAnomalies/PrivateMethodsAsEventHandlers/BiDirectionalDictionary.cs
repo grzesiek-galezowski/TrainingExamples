9bfc15fb2357ba42fcd74b9e7ab04d19f2638bb9
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-08-07 07:07:48 +0200

    Update BiDirectionalDictionary.cs

--- CSharp/EventAnomalies/PrivateMethodsAsEventHandlers/BiDirectionalDictionary.cs
+++ CSharp/EventAnomalies/PrivateMethodsAsEventHandlers/BiDirectionalDictionary.cs
@@ -9,23 +9,34 @@ namespace PrivateMethodsAsEventHandlers
       expiryPolicy.ItemNotNeededAnymore += this.OnItemNotNeededAnymore;
     }
 
-    private readonly Dictionary<string, string> _oneWay = new Dictionary<string, string>();
-    private readonly Dictionary<string, string> _otherWay = new Dictionary<string, string>();
+    private readonly Dictionary<string, string> _valuesByKey = new Dictionary<string, string>();
+    private readonly Dictionary<string, string> _keysByValue = new Dictionary<string, string>();
     
     public void Add(string key, string value)
     {
-      _oneWay[key] = value;
-      _otherWay[value] = key;
+      SetValueForKey(key, value);
+      SetKeyForValue(key, value);
     }
 
-    public string GetValueByKey(string key) => _oneWay[key];
-    public string GetKeyByValue(string value) => _otherWay[value];
+    private void SetValueForKey(string key, string value)
+    {
+      _valuesByKey[key] = value;
+    }
+
+    private void SetKeyForValue(string key, string value)
+    {
+      _keysByValue[value] = key;
+    }
+
+
+    public string GetValueByKey(string key) => _valuesByKey[key];
+    public string GetKeyByValue(string value) => _keysByValue[value];
 
     private void OnItemNotNeededAnymore(object sender, RemoveEventArgs eventArgs)
     {
-      var value = _oneWay[eventArgs.Key];
-      _oneWay.Remove(eventArgs.Key);
-      _otherWay.Remove(value);
+      var value = _valuesByKey[eventArgs.Key];
+      _valuesByKey.Remove(eventArgs.Key);
+      _keysByValue.Remove(value);
     }
 
   }
