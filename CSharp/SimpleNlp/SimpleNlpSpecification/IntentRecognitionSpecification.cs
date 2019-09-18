351b678177699b667e83645c3fec8f1f6ad4da02
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2019-09-18 19:51:10 +0200

    Update IntentRecognitionSpecification.cs

--- CSharp/SimpleNlp/SimpleNlpSpecification/IntentRecognitionSpecification.cs
+++ CSharp/SimpleNlp/SimpleNlpSpecification/IntentRecognitionSpecification.cs
@@ -120,11 +120,39 @@ namespace SimpleNlpSpecification
       recognitionResult3.TopIntent.Should().Be("INTENT_GAME_OVER");
       recognitionResult4.TopIntent.Should().Be("INTENT_START_OVER");
     }
+    
+    [Fact]
+    public void ShouldBeAbleToMatchMultipleIntentPatterns()
+    {
+      //GIVEN
+      var model = new Model();
+      
+      model.AddEntity(EntityName.Value("YES"), "yes");
+      model.AddEntity(EntityName.Value("NO"), "no");
+      model.AddEntity(EntityName.Value("START_OVER"), "start over");
+      model.AddEntity(EntityName.Value("GAME_OVER"), "game over");
+      model.AddIntent("INTENT", new[] { EntityName.Value("YES") });
+      model.AddIntent("INTENT", new[] { EntityName.Value("NO")});
+      model.AddIntent("INTENT", new[] { EntityName.Value("START_OVER")});
+      model.AddIntent("INTENT_GAME_OVER", new[] { EntityName.Value("GAME_OVER")});
 
-    //TODO matching multiple on any position
-    //bug intent not matched
-    //bug conflicts
-    //bug several alternatives for an intent
-    //bug what if no intent matches all entities?
+      //WHEN
+      var recognitionResult1 = model.Recognize("yes, please");
+      var recognitionResult2 = model.Recognize("no, thank you");
+      var recognitionResult3 = model.Recognize("game over, amigo");
+      var recognitionResult4 = model.Recognize("shall we start over?");
+
+      //THEN
+      recognitionResult1.TopIntent.Should().Be("INTENT");
+      recognitionResult2.TopIntent.Should().Be("INTENT");
+      recognitionResult3.TopIntent.Should().Be("INTENT_GAME_OVER");
+      recognitionResult4.TopIntent.Should().Be("INTENT");
+    }
+
+    //TODO approximate matching
+    //TODO intent not matched
+    //TODO conflicts
+    //TODO what if no intent matches all entities?
+    //TODO Intents with counts on entites (e.g. ENTITY_X, AtLeast(1.Times()))
   }
 }
