c3d8c99a4a923f90b50178a21c0b5b8a84de7cdd
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2020-02-12 00:13:39 +0100

    Update Age.cs

--- CSharp/ParseDoNotValidate/NUnitTestProject1/Age.cs
+++ CSharp/ParseDoNotValidate/NUnitTestProject1/Age.cs
@@ -5,27 +5,14 @@ namespace NUnitTestProject1
     public class Age : Value.ValueType<Age>
     {
         private readonly int _value;
-
-        private Age(in int value)
-        {
-            _value = value;
-        }
-
+        private Age(in int value) => _value = value;
         public int Value => _value;
+        public static Age Of(int value) => new Age(value);
+        public override string ToString() => _value.ToString();
 
         protected override IEnumerable<object> GetAllAttributesToBeUsedForEquality()
         {
             yield return _value;
-        }
-
-        public static Age Of(int value)
-        {
-            return new Age(value);
-        }
-
-        public override string ToString()
-        {
-          return _value.ToString();
         }
 
     }
