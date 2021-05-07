c7499a1e1959f60f219cca0e4bfc79fa47869305
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2021-05-07 11:30:45 +0200

    wip

---
+++ CSharp/AvoidAutoMockingContainers/AvoidAutoMockingContainers/AvoidAutoMockingContainers/README.txt
@@ -0,0 +1,12 @@
+﻿Potential pros of automocking:
+
+1. Decouples constructor from test (can be done better using builders)
+2. Makes test a bit shorter in a simple case
+
+Cons:
+1. Hides bloated constructor smell
+2. Obfuscates readability
+3. Turns pretty normal use cases into edge cases (which either further obfuscates readability or forces to rely on manual composition anyway)
+
+TODO: Fixture object example
+TODO: Builder example
--- CSharp/AvoidAutoMockingContainers/AvoidAutoMockingContainers/AvoidAutoMockingContainers/_04_EdgeCaseWithAutoMock.cs
+++ CSharp/AvoidAutoMockingContainers/AvoidAutoMockingContainers/AvoidAutoMockingContainers/_04_EdgeCaseWithAutoMock.cs
@@ -13,20 +13,23 @@ namespace AvoidAutoMockingContainers
     public class _04_EdgeCaseWithAutoMock
     {
         [Test]
-        public void ShouldRestartAllServicesPairwise()
+        public void ShouldRestartAllServicesPairwise_1()
         {
+            //I could not find a dedicated API in the automocking container to address this use case.
+            //Though there are at least three different ways to work around it...
+
             //GIVEN
             var timeout = Any.TimeSpan();
             using var autoMock = new AutoSubstitute(builder =>
             {
-                builder.Register(context => timeout).SingleInstance();
-                builder.Register(context => new List<IService>
+                builder.Register(_ => timeout).SingleInstance();
+                builder.Register(_ => new List<IService>
                 {
                     Substitute.For<IService>(),
                     Substitute.For<IService>(),
                     Substitute.For<IService>(),
                 }).SingleInstance().Named<List<IService>>("local");
-                builder.Register(context => new List<IService>
+                builder.Register(_ => new List<IService>
                 {
                     Substitute.For<IService>(),
                     Substitute.For<IService>(),
@@ -52,6 +55,38 @@ namespace AvoidAutoMockingContainers
                 autoMock.Container.ResolveNamed<List<IService>>("remote")[1].Restart(timeout);
                 autoMock.Container.ResolveNamed<List<IService>>("local")[2].Restart(timeout);
                 autoMock.Container.ResolveNamed<List<IService>>("remote")[2].Restart(timeout);
+            });
+        }
+
+        [Test] //we can use automocker like this, but... ;-)
+        public void ShouldRestartAllServicesPairwise_2()
+        {
+            //GIVEN
+            using var autoMock = new AutoSubstitute();
+            var localService1 = Substitute.For<IService>();
+            var localService2 = Substitute.For<IService>();
+            var localService3 = Substitute.For<IService>();
+            var remoteService1 = Substitute.For<IService>();
+            var remoteService2 = Substitute.For<IService>();
+            var remoteService3 = Substitute.For<IService>();
+            var timeout = Any.TimeSpan();
+            var sequences = autoMock.Resolve<RestartSequences>(
+                new PositionalParameter(0, new List<IService> {localService1, localService2, localService3}),
+                new PositionalParameter(1, new List<IService> {remoteService1, remoteService2, remoteService3}),
+                new PositionalParameter(2, timeout));
+
+            //WHEN
+            sequences.RestartAll();
+
+            //THEN
+            Received.InOrder(() =>
+            {
+                localService1.Restart(timeout);
+                remoteService1.Restart(timeout);
+                localService2.Restart(timeout);
+                remoteService2.Restart(timeout);
+                localService3.Restart(timeout);
+                remoteService3.Restart(timeout);
             });
         }
     }
