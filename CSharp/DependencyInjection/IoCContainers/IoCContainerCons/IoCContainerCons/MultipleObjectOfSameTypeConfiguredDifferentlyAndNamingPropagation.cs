c5b1b229e66351b396c8194a8f7c5d232ef79aba
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2021-06-27 22:15:12 +0200

    Update MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation.cs

--- CSharp/DependencyInjection/IoCContainers/IoCContainerCons/IoCContainerCons/MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation.cs
+++ CSharp/DependencyInjection/IoCContainers/IoCContainerCons/IoCContainerCons/MultipleObjectOfSameTypeConfiguredDifferentlyAndNamingPropagation.cs
@@ -10,26 +10,84 @@ namespace IoCContainerCons
     {
       //GIVEN
       var builder = new ContainerBuilder();
-      builder.RegisterType<A>().SingleInstance();
-      builder.RegisterType<A>().SingleInstance();
-      builder.RegisterType<B1>().SingleInstance();
-      builder.RegisterType<B1>().SingleInstance();
-      builder.RegisterType<C1>().SingleInstance();
-      builder.RegisterType<C1>().SingleInstance();
-      builder.RegisterType<C2>().SingleInstance();
-      builder.RegisterType<C2>().SingleInstance();
-      builder.RegisterType<B2>().SingleInstance();
-      builder.RegisterType<B2>().SingleInstance();
+      builder.RegisterType<O>()
+        .As<O>()
+        .WithParameter(
+          (info, context) => info.Position == 0,
+          (info, context) => context.ResolveNamed<A>("firstA"))
+        .WithParameter(
+          (info, context) => info.Position == 1,
+          (info, context) => context.ResolveNamed<A>("secondA"))
+        .SingleInstance();
+      
+      builder.RegisterType<A>().As<A>()
+        .WithParameter(
+          (info, context) => info.Position == 0,
+          (info, context) => context.ResolveNamed<B1>("firstB1")
+        )
+        .WithParameter(
+          (info, context) => info.Position == 1,
+          (info, context) => context.ResolveNamed<B2>("firstB2")
+        )
+        .Named<A>("firstA")
+        .SingleInstance();
+
+      builder.RegisterType<A>().As<A>()
+        .WithParameter(
+          (info, context) => info.Position == 0,
+          (info, context) => context.ResolveNamed<B1>("secondB1")
+          )
+        .WithParameter(
+          (info, context) => info.Position == 1,
+          (info, context) => context.ResolveNamed<B2>("secondB2")
+          )
+        .Named<A>("secondA")
+        .SingleInstance();
+
+      builder.RegisterType<B1>()        
+        .WithParameter(
+          (info, _) =>  info.Position == 0,
+          (_, context) => context.ResolveNamed<C1>("firstC1"))
+        .WithParameter(
+          (info, _) =>  info.Position == 1,
+          (_, context) => context.ResolveNamed<C2>("firstC2"))
+        .As<B1>().Named<B1>("firstB1").SingleInstance();
+
+      builder.RegisterType<B1>()
+        .WithParameter(
+          (info, _) =>  info.Position == 0,
+          (_, context) => context.ResolveNamed<C1>("secondC1"))
+        .WithParameter(
+          (info, _) =>  info.Position == 1,
+          (_, context) => context.ResolveNamed<C2>("secondC2"))
+        .As<B1>().Named<B1>("secondB1").SingleInstance();
+      
+      builder.RegisterType<C1>().Named<C1>("firstC1").SingleInstance();
+      builder.RegisterType<C1>().Named<C1>("secondC1").SingleInstance();
+      
+      builder.RegisterType<C2>().Named<C2>("firstC2").SingleInstance().WithParameter("X2", 2);
+      builder.RegisterType<C2>().Named<C2>("secondC2").WithParameter("X2", 4).SingleInstance();
+      
+      builder.RegisterType<B2>().Named<B2>("firstB2").SingleInstance().WithParameter("X1", 4);
+      builder.RegisterType<B2>().Named<B2>("secondB2").WithParameter("X1", 6).SingleInstance();
       using var container = builder.Build();
             
       //WHEN
-      var a1 = container.Resolve<A>();
+      var o = container.Resolve<O>();
+      
     
       //THEN
-      Assert.AreEqual(4, a1.B2.X1);
-      Assert.AreEqual(2, a1.B1.C2.X2);
-      //bug Assert.AreEqual(6, a2.B2.X1);
-      //bug Assert.AreEqual(4, a2.B1.C2.X2);
+      Assert.AreNotSame(o.A1, o.A2);
+      Assert.AreNotSame(o.A1.B1, o.A2.B1);
+      Assert.AreNotSame(o.A1.B1.C1, o.A2.B1.C1);
+      Assert.AreNotSame(o.A1.B1.C2, o.A2.B1.C2);
+      Assert.AreNotSame(o.A1.B1.C2.X2, o.A2.B1.C2.X2);
+      Assert.AreNotSame(o.A1.B2, o.A2.B2);
+
+      Assert.AreEqual(4, o.A1.B2.X1);
+      Assert.AreEqual(2, o.A1.B1.C2.X2);
+      Assert.AreEqual(6, o.A2.B2.X1);
+      Assert.AreEqual(4, o.A2.B1.C2.X2);
     }
 
     [Test]
@@ -37,16 +95,26 @@ namespace IoCContainerCons
     {
       //GIVEN
       //WHEN
-      var a1 = new A(new B1(new C1(), new C2(2)), new B2(4));
-      var a2 = new A(new B1(new C1(), new C2(4)), new B2(6));
+      var o = new O(
+        new A(
+          new B1(
+            new C1(),
+            new C2(2)),
+          new B2(4)),
+        new A(
+          new B1(
+            new C1(),
+            new C2(4)),
+          new B2(6)));
 
       //THEN
-      Assert.AreEqual(4, a1.B2.X1);
-      Assert.AreEqual(2, a1.B1.C2.X2);
-      Assert.AreEqual(6, a2.B2.X1);
-      Assert.AreEqual(4, a2.B1.C2.X2);
+      Assert.AreEqual(4, o.A1.B2.X1);
+      Assert.AreEqual(2, o.A1.B1.C2.X2);
+      Assert.AreEqual(6, o.A2.B2.X1);
+      Assert.AreEqual(4, o.A2.B1.C2.X2);
     }
 
+    public record O(A A1, A A2);
     public record A(B1 B1, B2 B2);
     public record B1(C1 C1, C2 C2);
     public record C2(int X2);
