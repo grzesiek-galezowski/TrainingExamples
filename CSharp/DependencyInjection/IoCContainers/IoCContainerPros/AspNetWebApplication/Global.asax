37d736f08c432e8f2fc5f220a4d1c00595aeb828
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2021-07-03 20:55:30 +0200

    wip

--- CSharp/DependencyInjection/IoCContainers/IoCContainerPros/AspNetWebApplication/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspNetWebApplication.Global" Language="C#" %>
--- CSharp/DependencyInjection/IoCContainers/IoCContainerPros/IoCContainerPros/_3_Interception.cs
+++ CSharp/DependencyInjection/IoCContainers/IoCContainerPros/IoCContainerPros/_3_Interception.cs
@@ -1,6 +1,4 @@
 using System;
-using System.IO;
-using System.Linq;
 using Autofac;
 using Autofac.Extras.DynamicProxy;
 using Castle.DynamicProxy;
