5cb284f66ab88300346df3509d2af72a0423d924
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2021-07-21 13:46:43 +0200

    update

--- CSharp/ModularMonolith/WarehouseModule/AppLogic/UpdateOrderStateCommand.cs
+++ CSharp/ModularMonolith/WarehouseModule/AppLogic/UpdateOrderStateCommand.cs
@@ -7,15 +7,18 @@ namespace WarehouseModule.AppLogic
 {
   public class UpdateOrderStateCommand
   {
-    private readonly LiteDbOrdersDao _ordersDao;
+    private readonly IOrdersDao _ordersDao;
     private readonly Guid _orderId;
     private readonly OrderStates _newState;
 
-    public UpdateOrderStateCommand(Guid orderId, OrderStates newState)
+    public UpdateOrderStateCommand(
+      Guid orderId,
+      OrderStates newState,
+      IOrdersDao dbOrdersDao)
     {
       _orderId = orderId;
       _newState = newState;
-      _ordersDao = new LiteDbOrdersDao();
+      _ordersDao = dbOrdersDao;
     }
 
     public async Task Execute(CancellationToken cancellationToken)
---
+++ CSharp/ModularMonolith/WarehouseModule/AppLogic/WarehouseCommandFactory.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace WarehouseModule.AppLogic
+{
+  public class WarehouseCommandFactory
+  {
+    private readonly ICustomerNotifications _customerNotifications;
+    private readonly IOrdersDao _createOrdersDao;
+
+    public WarehouseCommandFactory(
+      ICustomerNotifications customerNotifications,
+      IOrdersDao ordersDao)
+    {
+      _customerNotifications = customerNotifications;
+      _createOrdersDao = ordersDao;
+    }
+
+    public OrderDeliveryCommand CreateOrderDeliveryCommand(
+      ProductId productId,
+      string deliveryAddress,
+      string recipientEmailAddress)
+    {
+      return new OrderDeliveryCommand(
+        productId,
+        deliveryAddress,
+        recipientEmailAddress,
+        _createOrdersDao,
+        _customerNotifications);
+    }
+
+    public UpdateOrderStateCommand CreateUpdateOrderCommand(
+      Guid orderId,
+      OrderStates newState)
+    {
+      return new UpdateOrderStateCommand(orderId, newState, _createOrdersDao);
+    }
+  }
+}
