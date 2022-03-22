8a5128712082e83659102d354a9f489c9f8a2d8d
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-03-22 09:08:12 +0100

    Update StructuralTests.cs

--- CSharp/AntiAntiMock/AntiAntiMockSpecification/StructuralTests.cs
+++ CSharp/AntiAntiMock/AntiAntiMockSpecification/StructuralTests.cs
@@ -10,9 +10,12 @@ public class StructuralTests
     [Test]
     public void ShouldBeDecoratedWithTests()
     {
-        GetType().GetMethod(nameof(ShouldBeDecoratedWithTests)).Should().BeDecoratedWith<TestAttribute>();
-        GetType().GetMethod(nameof(ShouldBeDecoratedWithTests)).Should().NotBeAsync();
-        GetType().GetMethod(nameof(ShouldBeDecoratedWithTests)).Should().NotBeVirtual();
+        GetType().GetMethod(nameof(ShouldBeDecoratedWithTests))
+            .Should().BeDecoratedWith<TestAttribute>();
+        GetType().GetMethod(nameof(ShouldBeDecoratedWithTests))
+            .Should().NotBeAsync();
+        GetType().GetMethod(nameof(ShouldBeDecoratedWithTests))
+            .Should().NotBeVirtual();
     }
 
     [Test]
