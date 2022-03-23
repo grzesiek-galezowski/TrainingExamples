14d8c85698d9bb8cc05f21b3e506f9fa1303f09b
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-03-23 08:45:17 +0100

    Update _02_MocksVsHttpMocks.cs

--- CSharp/AntiAntiMock/AntiAntiMockSpecification/_01_ChangeDetectors/_02_MocksVsHttpMocks.cs
+++ CSharp/AntiAntiMock/AntiAntiMockSpecification/_01_ChangeDetectors/_02_MocksVsHttpMocks.cs
@@ -41,27 +41,27 @@ public class _02_MocksVsHttpMocks
   public async Task ShouldPassTheWorkToBothRecipientsInOrderViaHttp()
   {
     //GIVEN
-    using var mock1 = WireMockServer.Start();
-    mock1.Given(Request.Create())
+    using var recipient1 = WireMockServer.Start();
+    recipient1.Given(Request.Create())
       .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK));
-    using var mock2 = WireMockServer.Start();
-    mock2.Given(Request.Create())
+    using var recipient2 = WireMockServer.Start();
+    recipient2.Given(Request.Create())
       .RespondWith(Response.Create().WithStatusCode(HttpStatusCode.OK));
-    var mock1Url = mock1.Urls.Single() + "/";
-    var mock2Url = mock2.Urls.Single() + "/";
+    var mock1Url = recipient1.Urls.Single() + "/";
+    var mock2Url = recipient2.Urls.Single() + "/";
 
     await using var app = new WebApplicationFactory<Program>()
       .WithWebHostBuilder(
         builder => builder.ConfigureAppConfiguration((_, configurationBuilder)
           => configurationBuilder.AddInMemoryCollection(
-            new Dictionary<string, string> { ["Urls:Url1"] = mock1Url, ["Urls:Url2"] = mock2Url, })));
+            new Dictionary<string, string> { ["Urls:Url1"] = mock1Url, ["Urls:Url2"] = mock2Url })));
     using var client = new FlurlClient(app.CreateClient());
 
     //WHEN
     await client.Request("/broadcast").PostJsonAsync(new Work());
 
     //THEN
-    var logEntries = mock1.LogEntries.Concat(mock2.LogEntries).OrderBy(entry => entry.RequestMessage.DateTime).ToList();
+    var logEntries = recipient1.LogEntries.Concat(recipient2.LogEntries).OrderBy(entry => entry.RequestMessage.DateTime).ToList();
     logEntries[0].RequestMessage.AbsoluteUrl.Should().Be(mock1Url);
     logEntries[1].RequestMessage.AbsoluteUrl.Should().Be(mock2Url);
   }
