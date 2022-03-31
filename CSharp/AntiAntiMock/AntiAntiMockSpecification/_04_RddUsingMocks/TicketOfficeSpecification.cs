67a84451445fb25e318cce01fdb935e07605f99c
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-03-31 15:24:21 +0200

    Create TicketOfficeSpecification.cs

---
+++ CSharp/AntiAntiMock/AntiAntiMockSpecification/_04_RddUsingMocks/TicketOfficeSpecification.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NUnit.Framework;
+using TddXt.AnyRoot.Numbers;
+using static TddXt.AnyRoot.Root;
+
+namespace MockNoMockSpecification._04_RddUsingMocks;
+
+internal class TicketOfficeSpecification
+{
+  //[Test]
+  //public void ShouldReserveSeatsThereAreEnoughFreeSeatsInSpecifiedTrain()
+  //{
+  //  //GIVEN
+  //  var ticketOffice = new TicketOffice();
+  //  var trainId = Any.Instance<TrainId>();
+  //  var trainBooking = Substitute.For<ITrainBooking>();
+  //  var seatsCount = Any.UnsignedInt();
+  //  ticketOffice.AddBookingForTrain(trainId, trainBooking);
+  //  var customer = Substitute.For<ICustomer>();
+  //
+  //  trainBooking.HasFreeSeats(seatsCount).Returns(true); //bug what if false?
+  //
+  //  //WHEN
+  //  ticketOffice.IssueReservation(trainId, seatsCount, customer);
+  //
+  //  //THEN
+  //  trainBooking.Received(1).BookSeats(seatsCount, customer);
+  //}
+}
