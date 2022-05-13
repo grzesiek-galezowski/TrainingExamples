76d53b648f0f2d6d8b9c8f1f2010cbc0dc1032c5
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-05-13 09:50:56 +0200

    Update Program.cs

--- CSharp/ReturningResultsFromCommands/ReturningResultsFromCommands/Program.cs
+++ CSharp/ReturningResultsFromCommands/ReturningResultsFromCommands/Program.cs
@@ -4,17 +4,39 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+var logger = app.Services.GetRequiredService<ILogger<Program>>();
+
 app.MapPost("/todo1", () =>
 {
     var service = new TodoServiceReturningData(new FakeApi(), new FakeValidation());
     var result = service.AddTodo(new Todo("Do the slides"));
 
+    //ADVANTAGE: when dealing with the result, the logic is already done.
+    //ADVANTAGE: we always get a return value and always one (this is how return statement works :-) )
+    //DISADVANTAGE: when data is a request for a behavior, we need to interpret it to pick the right behavior
+    //DISADVANTAGE: mixing side-effects (logging, telemetry?) and pure functions
+    //              in the same piece of code (could be resolved via middleware?)
     return result.Match(
         success => Results.Ok(success.Id),
-        failure => failure.ErrorCode switch
+        failure =>
         {
-            ErrorCode.ApiCallFailed => Results.StatusCode(500),
-            ErrorCode.ValidationFailed => Results.BadRequest()
+            switch (failure.ErrorCode)
+            {
+                case ErrorCode.ApiCallFailed:
+                {
+                    logger.LogError($"Api call failed, {failure.Message}");
+                    return Results.StatusCode(500);
+                }
+                case ErrorCode.ValidationFailed:
+                {
+                    logger.LogError($"Validation failed, {failure.Message}");
+                    return Results.BadRequest();
+                }
+                default:
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
         });
 });
 
