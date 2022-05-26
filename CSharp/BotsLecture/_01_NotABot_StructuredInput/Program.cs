e470e64e6601e22581527c990aec7695de3e0b23
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-05-26 21:23:24 +0200

    Added bot lecture

---
+++ CSharp/BotsLecture/_01_NotABot_StructuredInput/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+public static class Program
+{
+  public static void Main(string[] args)
+  {
+    //try this:
+    //{ "Number": "ABC1234", "State": "Alabama" }
+    //{ "Number": "ABC1235", "State": "Alabama" }
+    //{ "Number": "ABC12", "State": "New York" }
+
+    while (true)
+    {
+      var text = Console.ReadLine();
+      var queryData = JsonSerializer.Deserialize<LicensePlateQueryData>(text);
+
+      var findCar = FindCar(queryData);
+      Console.WriteLine(
+        $"The car with license plate: {queryData.Number} in state {queryData.State} is " + findCar);
+
+    }
+  }
+
+  private static string FindCar(LicensePlateQueryData queryData)
+  {
+    return queryData switch
+    {
+      { State: "Alabama", Number: "ABC1234" } => "Alfa Romeo",
+      { State: "Alabama", Number: "ABC1235" } => "Cadillac",
+      { State: "New York", Number: "ABC12" } => "Rolls Royce",
+      _ => "Unknown"
+    };
+  }
+}
+
+public record LicensePlateQueryData(string Number, string State);
