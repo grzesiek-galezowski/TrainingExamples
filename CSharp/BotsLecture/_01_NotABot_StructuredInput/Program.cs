2b95fb09209fb54f2ce86a9574d2fd59b1a1d5f9
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-05-27 15:53:33 +0200

    Starting audio input

--- CSharp/BotsLecture/_01_NotABot_StructuredInput/Program.cs
+++ CSharp/BotsLecture/_01_NotABot_StructuredInput/Program.cs
@@ -1,56 +1,60 @@
 ﻿using System.Text.Json;
 
-public static class Program
+namespace _01_NotABot_StructuredInput
 {
-  public static void Main(string[] args)
+  public static class Program
   {
-    //try this:
-    //{ "Intent": "Plate", "Number": "ABC1234", "State": "Alabama" }
-    //{ "Intent": "Plate", "Number": "ABC1235", "State": "Alabama" }
-    //{ "Intent": "Plate", "Number": "ABC12", "State": "New York" }
-
-    while (true)
+    public static void Main(string[] args)
     {
-      try
+      //try this:
+      //{ "Intent": "Plate", "Number": "ABC1234", "State": "Alabama" }
+      //{ "Intent": "Plate", "Number": "ABC1235", "State": "Alabama" }
+      //{ "Intent": "Plate", "Number": "ABC12", "State": "New York" }
+
+      while (true)
       {
-        var text = Console.ReadLine();
-        var indentData = JsonSerializer.Deserialize<IntentData>(text);
-
-        if (indentData is { Intent: "Plate" })
+        try
         {
-          var queryData = JsonSerializer.Deserialize<LicensePlateQueryData>(text);
+          var text = Console.ReadLine();
+          var indentData = JsonSerializer.Deserialize<IntentData>(text);
 
-          var carMake = FindCar(queryData);
+          if (indentData is { Intent: "Plate" })
+          {
+            var queryData = JsonSerializer.Deserialize<LicensePlateQueryData>(text);
 
-          Console.WriteLine(
-            "The car with " +
-            $"license plate: {queryData.Number} " +
-            $"in state {queryData.State} " +
-            $"is {carMake}");
+            var carMake = FindCar(queryData);
+
+            Console.WriteLine(
+              "The car with " +
+              $"license plate: {queryData.Number} " +
+              $"in state {queryData.State} " +
+              $"is {carMake}");
+          }
+          else
+          {
+            Console.WriteLine("Sorry, I don't know what you mean");
+          }
         }
-        else
+        catch(Exception e)
         {
-          Console.WriteLine("Sorry, I don't know what you mean");
+          Console.WriteLine("Invalid input!");
+          Console.WriteLine(e.Message);
         }
       }
-      catch
+    }
+
+    private static string FindCar(LicensePlateQueryData queryData)
+    {
+      return queryData switch
       {
-        Console.WriteLine("Invalid input!");
-      }
+        { State: "Alabama", Number: "ABC1234" } => "Alfa Romeo",
+        { State: "Alabama", Number: "ABC1235" } => "Cadillac",
+        { State: "New York", Number: "ABC12" } => "Rolls Royce",
+        _ => "Unknown"
+      };
     }
   }
 
-  private static string FindCar(LicensePlateQueryData queryData)
-  {
-    return queryData switch
-    {
-      { State: "Alabama", Number: "ABC1234" } => "Alfa Romeo",
-      { State: "Alabama", Number: "ABC1235" } => "Cadillac",
-      { State: "New York", Number: "ABC12" } => "Rolls Royce",
-      _ => "Unknown"
-    };
-  }
+  public record IntentData(string Intent);
+  public record LicensePlateQueryData(string Number, string State);
 }
-
-public record IntentData(string Intent);
-public record LicensePlateQueryData(string Number, string State);
--- CSharp/BotsLecture/_02_UnstructuredInput/Program.cs
+++ CSharp/BotsLecture/_02_UnstructuredInput/Program.cs
@@ -1,77 +1,79 @@
 ﻿using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
 using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.Models;
 using Newtonsoft.Json.Linq;
-using static System.Environment.SpecialFolder;
-using ApiKeyServiceClientCredentials = Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime.ApiKeyServiceClientCredentials;
 
-public static class Program
+namespace _02_UnstructuredInput
 {
-  public static async Task Main(string[] args)
+  public static class Program
   {
-    while (true)
+    public static async Task Main(string[] args)
     {
-      try
+      while (true)
       {
-        var text = Console.ReadLine();
-        var data = await GetFromLuis(text);
-
-        if (data.TopIntent == "Plate")
+        try
         {
-          var queryData = GetEntitiesFrom(data);
+          var text = Console.ReadLine();
+          var data = await GetStructuredOutputFrom(text);
 
-          var carMake = FindCar(queryData);
+          if (data.TopIntent == "Plate")
+          {
+            var queryData = GetEntitiesFrom(data);
 
-          Console.WriteLine(
-            "The car with " +
-            $"license plate: {queryData.Number} " +
-            $"in state {queryData.State} " +
-            $"is {carMake}");
+            var carMake = FindCar(queryData);
+
+            Console.WriteLine(
+              "The car with " +
+              $"license plate: {queryData.Number} " +
+              $"in state {queryData.State} " +
+              $"is {carMake}");
+          }
+          else
+          {
+            Console.WriteLine("Sorry, I don't know what you mean");
+          }
         }
-        else
+        catch(Exception e)
         {
-          Console.WriteLine("Sorry, I don't know what you mean");
+          Console.WriteLine("Invalid input!");
+          Console.WriteLine(e.Message);
         }
       }
-      catch
+    }
+
+    private static LicensePlateQueryData GetEntitiesFrom(Prediction data)
+    {
+      return new LicensePlateQueryData(
+        ((JArray)data.Entities["PlateNumber"]).First.ToString(),
+        ((JArray)data.Entities["State"]).First.ToString());
+    }
+
+    private static async Task<Prediction> GetStructuredOutputFrom(string text)
+    {
+      var luisKey = await File.ReadAllTextAsync($"{Environment.SpecialFolder.MyDocuments}\\__KEYS\\luis.txt");
+      var luisAppId = await File.ReadAllTextAsync($"{Environment.SpecialFolder.MyDocuments}\\__KEYS\\luisApp.txt");
+      var luisAppUrl = await File.ReadAllTextAsync($"{Environment.SpecialFolder.MyDocuments}\\__KEYS\\luisAppUrl.txt");
+      var credentials = new ApiKeyServiceClientCredentials(luisKey);
+      var runtimeClient 
+        = new LUISRuntimeClient(credentials) { Endpoint = luisAppUrl };
+      var result = await runtimeClient.Prediction.GetSlotPredictionWithHttpMessagesAsync(
+        Guid.Parse(luisAppId), 
+        "staging", 
+        new PredictionRequest(text));
+
+      return result.Body.Prediction;
+    }
+
+    private static string FindCar(LicensePlateQueryData queryData)
+    {
+      return queryData switch
       {
-        Console.WriteLine("Invalid input!");
-      }
+        { State: "Alabama", Number: "ABC1234" } => "Alfa Romeo",
+        { State: "Alabama", Number: "ABC1235" } => "Cadillac",
+        { State: "New York", Number: "ABC12" } => "Rolls Royce",
+        _ => "Unknown"
+      };
     }
   }
 
-  private static LicensePlateQueryData GetEntitiesFrom(Prediction data)
-  {
-    return new LicensePlateQueryData(
-      ((JArray)data.Entities["PlateNumber"]).First.ToString(),
-      ((JArray)data.Entities["State"]).First.ToString());
-  }
-
-  private static async Task<Prediction> GetFromLuis(string text)
-  {
-    var luisKey = await File.ReadAllTextAsync($"{MyDocuments}\\__KEYS\\luis.txt");
-    var luisAppId = await File.ReadAllTextAsync($"{MyDocuments}\\__KEYS\\luisApp.txt");
-    var luisAppUrl = await File.ReadAllTextAsync($"{MyDocuments}\\__KEYS\\luisAppUrl.txt");
-    var credentials = new ApiKeyServiceClientCredentials(luisKey);
-    var runtimeClient 
-      = new LUISRuntimeClient(credentials) { Endpoint = luisAppUrl };
-    var result = await runtimeClient.Prediction.GetSlotPredictionWithHttpMessagesAsync(
-      Guid.Parse(luisAppId), 
-      "staging", 
-      new PredictionRequest(text));
-
-    return result.Body.Prediction;
-  }
-
-  private static string FindCar(LicensePlateQueryData queryData)
-  {
-    return queryData switch
-    {
-      { State: "Alabama", Number: "ABC1234" } => "Alfa Romeo",
-      { State: "Alabama", Number: "ABC1235" } => "Cadillac",
-      { State: "New York", Number: "ABC12" } => "Rolls Royce",
-      _ => "Unknown"
-    };
-  }
+  public record LicensePlateQueryData(string Number, string State);
 }
-
-public record LicensePlateQueryData(string Number, string State);
