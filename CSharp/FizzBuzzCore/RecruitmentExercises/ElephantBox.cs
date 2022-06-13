a3d2c4cde75eb9fa3f5fb93f2fecdc35d10d3dd2
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2022-06-13 23:08:21 +0200

    Added elephants exercise

---
+++ CSharp/FizzBuzzCore/RecruitmentExercises/ElephantBox.cs
@@ -0,0 +1,37 @@
+public interface IContainable
+{
+  //Propose method inherited by both Elephant
+  //and Box that will allow getting the elephant count
+}
+
+class Elephant : IContainable
+{
+}
+
+class Box : IContainable
+{
+  private readonly IReadOnlyList<IContainable> _content;
+
+  public Box(params IContainable[] content)
+  {
+    _content = content;
+  }
+}
+
+
+public class Tests
+{
+  [Test]
+  public void Example()
+  {
+    //Elephant count should be 3
+    IContainable box = new Box(
+      new Box(
+        new Elephant()),
+      new Box(
+        new Box(
+          new Elephant(),
+          new Box(
+            new Elephant()))));
+  }
+}
