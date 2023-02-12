15c4b8d0db10b6d7aeef7d5f01b2b0464e3de86d
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2023-02-12 22:56:59 +0100

    wip

--- CSharp/AdapterTests/AdapterTests/UnitTest1.cs
+++ CSharp/AdapterTests/AdapterTests/UnitTest1.cs
@@ -1,11 +1,17 @@
 using Extensions.Logging.NUnit;
 using FluentAssertions;
 using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
 using GreetingService.Services;
 using Grpc.Net.Client;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Logging;
 using NVidiaAsr;
+using TddXt.AnyExtensibility;
+using TddXt.AnyRoot;
+using TddXt.TypeResolution;
+using TddXt.TypeResolution.FakeChainElements.InlineGeneratorTypes.Generic.ImplementationDetails;
+using static TddXt.AnyRoot.Root;
 
 namespace AdapterTests;
 
@@ -14,6 +20,16 @@ public class Tests
   [Test]
   public async Task Test1()
   {
+    var withDefaultLimits = DefaultGenerationRequest.WithDefaultLimits();
+    var x = ((AllGenerator)Root.Any).Instance<SpeechRecognitionResult>(withDefaultLimits);
+
+    var y = new SpeechRecognitionResult()
+    {
+      Alternatives = { new SpeechRecognitionAlternative() }
+    }
+
+    x.Alternatives.Count.Should().Be(3);
+    x.AudioProcessed.Should().NotBe(0);
     await using var factory = new WebApplicationFactory<AsrService>();
     var loggerFactory = new LoggerFactory();
     loggerFactory.AddProvider(new NUnitLoggerProvider());
