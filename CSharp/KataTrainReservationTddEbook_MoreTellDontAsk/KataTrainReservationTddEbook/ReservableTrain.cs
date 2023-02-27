011b70d9ff8bc6ca1c8f2a18f86cbe2e4a32c93d
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2023-02-27 23:33:47 +0100

    wip

--- CSharp/KataTrainReservationTddEbook_MoreTellDontAsk/KataTrainReservationTddEbook/ReservableTrain.cs
+++ CSharp/KataTrainReservationTddEbook_MoreTellDontAsk/KataTrainReservationTddEbook/ReservableTrain.cs
@@ -22,43 +22,6 @@ public class ReservableTrain : ITrain, IReservableTrain
     throw new System.NotImplementedException();
   }
 
-  public bool HasEnoughPlaceInAnyIndividualCoachFor(uint requestedSeatCount)
-  {
-    return _coaches.Any(c => c.HasPlaceFor(requestedSeatCount));
-  }
-
-  public Maybe<ICoach> GetCoachWithEnoughPlaceFor(uint requestedSeatCount)
-  {
-    return _coaches.FirstOrDefault(c => c.HasPlaceFor(requestedSeatCount)).ToMaybe();
-  }
-
-  public bool HasTotalUpFrontFreeSeatsAtLeast(uint requestedSeatCount)
-  {
-    var reservableSeats = _totalSeats * 0.7;
-    var freeSeats = reservableSeats - _reservedSeats;
-    return requestedSeatCount <= freeSeats;
-  }
-
-  public bool HasACoachThatWouldNotExceedTheSoftLimit(uint requestedSeatCount)
-  {
-    return _coaches.Any(c => c.HasPlaceWithingTheSoftLimitFor(requestedSeatCount));
-  }
-
-  public void BookSeatsInTheFirstCoachThatDoesNotExceedSoftLimitAfterBooking(
-    uint requestedSeatCount,
-    IReservationInProgress reservationInProgress)
-  {
-    _coaches.First(c => c.HasPlaceWithingTheSoftLimitFor(requestedSeatCount))
-      .Reserve(requestedSeatCount, reservationInProgress);
-  }
-
-  public void BookSeatsInTheFirstCoachThatHasEnoughSeatsFor(uint requestedSeatCount,
-    IReservationInProgress reservationInProgress)
-  {
-    _coaches.First(c => c.HasPlaceFor(requestedSeatCount))
-      .Reserve(requestedSeatCount, reservationInProgress);
-  }
-
   public void EvaluateIndividualCoachReservability(uint requestedSeatCount, IReservationInProgress reservationInProgress,
     CoachReservabilityVisitor coachReservabilityVisitor)
   {
@@ -87,7 +50,6 @@ public class ReservableTrain : ITrain, IReservableTrain
     {
       coachSoftLimitReservabilityVisitor.CoachNotFound(requestedSeatCount, reservationInProgress, this);
     }
-
   }
 
   public void EvaluateUpFrontTrainReservability(uint requestedSeatCount, IReservationInProgress reservationInProgress,
@@ -106,5 +68,17 @@ public class ReservableTrain : ITrain, IReservableTrain
   private Maybe<ICoach> GetCoachWithEnoughPlaceWithinSoftLimitFor(uint requestedSeatCount)
   {
     return _coaches.FirstOrDefault(c => c.HasPlaceWithingTheSoftLimitFor(requestedSeatCount)).ToMaybe();
+  }
+
+  private Maybe<ICoach> GetCoachWithEnoughPlaceFor(uint requestedSeatCount)
+  {
+    return _coaches.FirstOrDefault(c => c.HasPlaceFor(requestedSeatCount)).ToMaybe();
+  }
+
+  private bool HasTotalUpFrontFreeSeatsAtLeast(uint requestedSeatCount)
+  {
+    var reservableSeats = _totalSeats * 0.7;
+    var freeSeats = reservableSeats - _reservedSeats;
+    return requestedSeatCount <= freeSeats;
   }
 }
--- CSharp/KataTrainReservationTddEbook_MoreTellDontAsk/KataTrainReservationTddEbook/Usings.cs
+++ CSharp/KataTrainReservationTddEbook_MoreTellDontAsk/KataTrainReservationTddEbook/Usings.cs
@@ -2,7 +2,5 @@
 
 global using FluentAssertions;
 global using NSubstitute;
-global using TddXt.AnyRoot.Collections;
-global using TddXt.AnyRoot.Numbers;
 global using Xunit;
 global using static TddXt.AnyRoot.Root;
