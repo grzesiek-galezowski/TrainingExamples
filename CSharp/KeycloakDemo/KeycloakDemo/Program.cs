95e5138137f71b090dc4e98981470c2674b92600
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2023-09-05 22:59:56 +0200

    Added keycloak playground

---
+++ CSharp/KeycloakDemo/KeycloakDemo/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using FluentAssertions;
+using IdentityModel.Client;
+using Keycloak.Net.Models.Clients;
+using Keycloak.Net.Models.ClientScopes;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.TestHost;
+using Testcontainers.Keycloak;
+
+namespace KeycloakDemo
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var appFactory = new WebApplicationFactory<SecureApp.Program>()
+                .WithWebHostBuilder(b => b.UseTestServer(o => o.PreserveExecutionContext = true));
+
+            var connection = new HubConnectionBuilder().WithUrl(
+                "http://localhost/echo",
+                o => o.HttpMessageHandlerFactory = _ => appFactory.Server.CreateHandler()).Build();
+
+            await connection.StartAsync();
+
+            await using var container = new KeycloakBuilder().Build();
+            await container.StartAsync();
+
+            var keycloakClient = new Keycloak.Net.KeycloakClient(
+                container.GetBaseAddress(),
+                "admin",
+                "admin");
+
+            (await keycloakClient.CreateClientScopeAsync("master", new ClientScope()
+            {
+                Id = "scope1",
+                Name = "scope1"
+            })).Should().BeTrue();
+
+            (await keycloakClient.CreateClientAsync("master", new Client()
+            {
+                ClientId = "zenek",
+                Access = new ClientAccess()
+                {
+                    Configure = true,
+                    Manage = true,
+                    View = true
+                },
+                Enabled = true,
+                Secret = "zenon",
+                DefaultClientScopes = new List<string>() { "scope1", "scope2", "scope3"},
+                ServiceAccountsEnabled = true,
+                AuthorizationServicesEnabled = true
+            })).Should().BeTrue();
+
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetDiscoveryDocumentAsync(
+                container.GetBaseAddress() + "realms/master");
+            if (response.HttpStatusCode != HttpStatusCode.OK) 
+                throw new Exception(response.HttpErrorReason);
+            if (response.IsError) 
+                throw new Exception(response.ErrorType + " " + response.Error);
+
+            var tokenResponse = await httpClient.RequestClientCredentialsTokenAsync(
+                new ClientCredentialsTokenRequest()
+            {
+                Address = response.TokenEndpoint,
+                ClientId = "zenek",
+                ClientSecret = "zenon",
+                Scope = "openid",
+            });
+
+            Console.WriteLine(tokenResponse.HttpStatusCode);
+            Console.WriteLine(tokenResponse.ErrorDescription ?? "no description");
+            Console.WriteLine(tokenResponse.AccessToken ?? "no token");
+            Console.WriteLine(tokenResponse.HttpErrorReason ?? "no reason");
+        }
+    }
+}
---
+++ CSharp/KeycloakDemo/SecureApp/Program.cs
@@ -0,0 +1,66 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SecureApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                builder.Configuration.Bind(options);
+                options.Authority = "Authority URL"; // TODO: Update URL
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        if (!string.IsNullOrEmpty(accessToken))
+                        {
+                            context.Token = accessToken;
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+            builder.Services.AddAuthorization();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapHub<MyEchoHub>("echo");
+
+            app.Run();
+        }
+    }
+
+    public class MyEchoHub : Hub<IEcho>
+    {
+    }
+
+    public interface IEcho
+    {
+    }
+}
