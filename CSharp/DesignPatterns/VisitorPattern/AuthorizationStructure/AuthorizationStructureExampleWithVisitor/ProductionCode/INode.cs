4bb48ff71e1e69b993b01ae15ba5c07297408b5e
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-02-23 23:03:22 +0100

    Update INode.cs

--- CSharp/DesignPatterns/VisitorPattern/AuthorizationStructure/AuthorizationStructureExampleWithVisitor/ProductionCode/INode.cs
+++ CSharp/DesignPatterns/VisitorPattern/AuthorizationStructure/AuthorizationStructureExampleWithVisitor/ProductionCode/INode.cs
@@ -3,7 +3,6 @@ namespace AuthorizationStructureExampleWithVisitor.ProductionCode;
 public interface INode
 {
   void Accept(INodeVisitor visitor);
-  //void Accept(INodeHierarchyVisitor visitor); //bug
   void Dump(IChangeEventsTarget target);
   void RemoveFrom(Dictionary<NodeId, INode> nodesById, IChangeEventsTarget eventsTarget);
   void UnplugFromParent();
